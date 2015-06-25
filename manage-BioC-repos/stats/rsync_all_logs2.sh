95f39f619a7bdd404d8108198d1e0f127c3481fa
hpages@fhcrc.org <hpages@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-06-25 00:36:59 +0000

    adjust paths to logs
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/BBS@105457 bc3139a8-67e5-0310-9ffc-ced21a209358

--- manage-BioC-repos/stats/rsync_all_logs2.sh
+++ manage-BioC-repos/stats/rsync_all_logs2.sh
@@ -4,28 +4,29 @@
 set -e  # Exit immediately if a simple command exits with a non-zero status
 
 STATS_HOME=/home/biocadmin/STATS
-#SERVER_LOGS_PATH=biocadmin@gopher6:/home/server_logs
-SERVER_LOGS_PATH=biocadmin@rhino1:server_logs
+LOGS_LOCAL_PATH=$STATS_HOME/bioc-access-logs
+#LOGS_REMOTE_PATH=biocadmin@gopher6:/home/server_logs
+LOGS_REMOTE_PATH=biocadmin@rhino01:server_logs
 
-cd $STATS_HOME
+cd $LOGS_LOCAL_PATH
 echo ""
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo "> RETRIEVING SQUID LOG FILES FROM $SERVER_LOGS_PATH/cobra"
+echo "> RETRIEVING SQUID LOG FILES FROM $LOGS_REMOTE_PATH/cobra"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo ""
-rsync -ave ssh "$SERVER_LOGS_PATH/cobra/squid/access.log*" $STATS_HOME/bioc-access-logs/squid/cobra
-
-echo ""
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo "> RETRIEVING SQUID LOG FILES FROM $SERVER_LOGS_PATH/mamba"
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo ""
-rsync -ave ssh "$SERVER_LOGS_PATH/mamba/squid/access.log*" $STATS_HOME/bioc-access-logs/squid/mamba
+rsync -ave ssh "$LOGS_REMOTE_PATH/cobra/squid/access.log*" $LOGS_LOCAL_PATH/squid/cobra
 
 echo ""
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo "> RETRIEVING APACHE2 LOG FILES FROM $SERVER_LOGS_PATH/krait"
+echo "> RETRIEVING SQUID LOG FILES FROM $LOGS_REMOTE_PATH/mamba"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo ""
-rsync -ave ssh "$SERVER_LOGS_PATH/krait/apache2/bioconductor-access.log-*" $STATS_HOME/bioc-access-logs/apache2/krait
+rsync -ave ssh "$LOGS_REMOTE_PATH/mamba/squid/access.log*" $LOGS_LOCAL_PATH/squid/mamba
+
+echo ""
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo "> RETRIEVING APACHE2 LOG FILES FROM $LOGS_REMOTE_PATH/krait"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo ""
+rsync -ave ssh "$LOGS_REMOTE_PATH/krait/apache2/bioconductor-access.log-*" $LOGS_LOCAL_PATH/apache2/krait
 
