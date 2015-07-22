b5b6659cd36dec40359608afd69195e82cfb1a10
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-07-22 16:54:52 +0000

    Add script to check whether mirrors support https.
    From: Dan Tenenbaum <dtenenba@fhcrc.org>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/BBS@106665 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ utils/checkHttps.R
@@ -0,0 +1,51 @@
+
+if (!require(BiocInstaller))
+  source("http://bioconductor.org/biocLite.R")
+
+deps <- c("httr", "yaml", "R.utils")
+
+for (dep in deps)
+{
+    if (!do.call(require, list(dep)))
+    {
+        biocLite(dep)
+        do.call(library, list(dep))
+    }
+}
+
+defaultMirror <- function()
+{
+    unname(sub("http:", "https:", biocinstallRepos()["BioCsoft"], TRUE))
+}
+
+testHttps <- function(mirrorUrl=defaultMirror())
+{
+    oldwarn <- getOption('warn')
+    on.exit(options(warn=oldwarn))
+    options(warn=2)
+    print(sprintf("Trying %s ...", mirrorUrl))
+    tryCatch({
+        withTimeout(download.packages("Biobase", tempdir(), repos=mirrorUrl),
+            timeout=30, onTimeout="error")
+        TRUE
+        },
+        error=function(e) FALSE)
+}
+
+getMirrors <- function()
+{
+    yaml <- content(GET("http://bioconductor.org/config.yaml"))
+    obj <- yaml.load(yaml)
+    mirrors <- unlist(lapply(obj$mirrors, function(x) {
+        x=unlist(unname(x))
+        unlist(unname(x[names(x)=="mirror_url"]))
+    }))
+    mirrors <- sub("http:", "https:", mirrors, TRUE)
+    mirrors <- paste0(mirrors, "packages/", biocVersion(), "/", "bioc")
+    mirrors <- c(defaultMirror(), mirrors)
+    names(mirrors) <- mirrors
+    mirrors
+}
+
+
+unlist(lapply(getMirrors(), testHttps))
