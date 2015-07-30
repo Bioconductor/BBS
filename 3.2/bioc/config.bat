45e4303e91c3f9464cff0fb70e8b18c8bc5c08c1
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-07-30 13:08:01 -0700

    set master builder to linux1 (in more places)

--- 3.2/bioc/config.bat
+++ 3.2/bioc/config.bat
@@ -13,7 +13,7 @@ set BBS_BIOC_MANIFEST_FILE=bioc_3.2.manifest
 set BBS_MEAT0_TYPE=1
 
 @rem Where is it?
-set BBS_MEAT0_RHOST=zin1
+set BBS_MEAT0_RHOST=linux1
 set BBS_MEAT0_RUSER=biocbuild
 set BBS_MEAT0_RDIR=/home/biocbuild/bbs-3.2-bioc/MEAT0
 
