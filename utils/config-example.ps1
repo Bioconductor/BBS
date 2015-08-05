02159c076fdff36a967c2ca0e4b761f828fb7d3a
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-08-05 14:04:00 -0700

    add gtk+, gtkmm

---
+++ utils/config-example.ps1
@@ -0,0 +1,21 @@
+<#
+Example configuration values.
+
+Do NOT put actual config values in this file.
+
+Copy it to config.ps1 and set the actual values there.
+Do NOT check config.ps1 into version control!
+Values are security-sensitive (passwords, etc.) so
+be mindful of that.
+
+DELETE config.ps1 when you are done with it!
+#>
+
+$Env:COMPUTER_NAME = "windows1"
+$Env:BIOCBUILD_PASSWORD="secret!!!"
+$Env:BIOC_VERSION="3.2"
+$Env:RTOOLS_VERSION="33"
+$Env:R_URL="https://cran.rstudio.com/bin/windows/base/R-3.2.2beta-win.exe"
+$Env:R_INSTALLER="R-3.2.2beta-win.exe"
+$Env:USE_DEVEL="TRUE"
+$Env:GOOGLE_API_KEY="secret!!"
--- utils/setup-win.ps1
+++ utils/setup-win.ps1
@@ -1,15 +1,27 @@
-# prerequisites:
-# a windows server 2012 r2 machine with
-# cygwin installed, specifically rsync, curl, and ssh
-# (and optionally vim) and in the path
+<#
+prerequisites:
+a windows server 2012 r2 machine with
+cygwin installed, specifically rsync, curl, and ssh
+(and optionally vim) and in the path
 
-# COMPUTER_NAME should be set 
-# to the name this machine should have
-# (without the domain name)
+COMPUTER_NAME should be set 
+to the name this machine should have
+(without the domain name)
 
-# if you are going to run this as a script, you need
-# to run this line in a powershell window first:
-# Set-ExecutionPolicy RemoteSigned
+if you are going to run this as a script, you need
+to run this line in a powershell window first:
+Set-ExecutionPolicy RemoteSigned
+
+Before running this, you should make another
+script based on config-example.ps1 (in this 
+directory) called config.ps1, edit the values
+to match your system, then source it like this:
+
+. .\config.ps1
+
+Then run this script.
+
+#>
 
 # rename computer; this requires a reboot to take effect ;-(
 Rename-Computer -NewName $Env:COMPUTER_NAME
@@ -69,7 +81,6 @@ mkdir \biocbld\.BBS
 
 echo "You have to manually add c:\.BBS\id_rsa!!!!"
 
-# be sure BIOCBUILD_PASSWORD env var is set....
 net user biocbuild "$Env:BIOCBUILD_PASSWORD" /ADD
 
 # give biocbuild the right to log on 
@@ -89,36 +100,35 @@ $path += ";c:\Python27"
 # set it locally so we don't have to wait for a restart:
 $Env:Path = $path
 
-# be sure BIOC_VERSION variable is set
 
 #cd "c:\biocbld\BBS\$Env:BIOC_VERSION"
 
 # no point, SET statements in bat file are ignored....
 
-# be sure RTOOLS_VERSION is set to e.g. 33
 
 iex "$curl -LO https://cran.rstudio.com/bin/windows/Rtools/Rtools$env:RTOOLS_VERSION.exe"
 
-siex " .\Rtools$env:RTOOLS_VERSION.exe /verysilent"
+iex " .\Rtools$env:RTOOLS_VERSION.exe /verysilent"
 
- $path = "C:\Rtools\bin;C:\Rtools\gcc-4.6.3\bin;" + $path
+$path = "C:\Rtools\bin;C:\Rtools\gcc-4.6.3\bin;" + $path
 
 [Environment]::SetEnvironmentVariable("PATH", $path, "Machine")
 
 # set it locally so we don't have to wait for a restart:
 $Env:Path = $path
 
-# be sure BIOC_VERSION is set to e.g. 3.2
 $bioc_basedir = "c:\biocbld\bbs-$env:BIOC_VERSION-bioc"
 
 mkdir $bioc_basedir
 
+cd $bioc_basedir
 
-# be sure R_URL is set to the URL containing the R for 
-# windows installer
+mkdir log
+# mkdir tmp 
+# mkdir tmpdir
+# mkdir meat
 
-# be sure R_INSTALLER is set to e.g.
-# R-3.2.1patched-win.exe
+cd $download_dir
 
 $r_basedir = "$bioc_basedir\R"
 $r = "$r_basedir\bin\R"
@@ -136,8 +146,6 @@ iex ".\$env:R_INSTALLER /DIR=$r_basedir /noicons /verysilent"
 #iex "$r -e `"source('http://bioconductor.org/biocLite.R')`""
 Start-Process $r -Credential biocbuild -ArgumentList "-e `"source('http://bioconductor.org/biocLite.R')`""
 
-# be sure USE_DEVEL is set to TRUE or FALSE
-
 # useDevel() if appropriate; this will prompt (again?)
 # for biocbuild's password
 if ($env:USE_DEVEL -eq "TRUE") {Start-Process $r -Credential biocbuild -ArgumentList "-e BiocInstaller::useDevel()"}
@@ -146,7 +154,6 @@ if ($env:USE_DEVEL -eq "TRUE") {Start-Process $r -Credential biocbuild -Argument
 # could be put in its own script and that script could be run
 # as biocbuild, so biocbuild's password only needs to be typed
 # once.
-
 
 # download + install MikTex
 iex "$curl -LO http://mirrors.ctan.org/systems/win32/miktex/setup/basic-miktex-2.9.5105.exe"
@@ -162,7 +169,6 @@ $Env:Path = $path
 
 [Environment]::SetEnvironmentVariable("MIKTEX_ENABLEWRITE18", "t", "Machine")
 
-# fixme - put all env vars in config (w/example) ps1 script
 
 iex "$curl -LO http://s3.amazonaws.com/bioc-windows-setup/jdk-8u51-windows-x64.exe"
 
@@ -184,4 +190,63 @@ $Env:Path = $path
 ## seems like javareconf is no longer necessary?
 ##Start-Process $r -Credential biocbuild -ArgumentList "CMD javareconf"
 
+[Environment]::SetEnvironmentVariable("GOOGLE_API_KEY", $Env:GOOGLE_API_KEY, "Machine")
 
+#### INSTALL SYSTEM DEPS
+
+# ggobi
+
+iex "$curl -LO http://www.ggobi.org/downloads/ggobi-2.1.10.exe"
+
+# this automatically adds it to the path...
+iex ".\ggobi-2.1.10.exe /S"
+
+# ... so we need to get the path anew:
+
+$path = [Environment]::GetEnvironmentVariable("PATH","Machine")
+
+
+# FIXME - get ^^ to install w/o creating a desktop shortcut
+
+# ...but we still need to do this:
+
+[Environment]::SetEnvironmentVariable("GGOBI_HOME", "c:/progra~2/ggobi", "Machine")
+[Environment]::SetEnvironmentVariable("GGOBI_PATH", "c:/progra~2/ggobi", "Machine")
+
+# this is really useful for figuring out how to
+# run various installers non-interactively:
+# http://unattended.sourceforge.net/installers.php
+
+# GTK+
+
+# a. 32 bit
+
+iex "$curl -LO http://win32builder.gnome.org/packages/3.6/gtk+_3.6.4-1_win32.zip"
+
+unzip gtk+_3.6.4-1_win32.zip -d c:\gtk+_3.6.4-1
+
+[Environment]::SetEnvironmentVariable("GTK_BASEPATH", "c:/gtk__3~1.4-1", "Machine")
+
+$path += ";C:\gtk+_3.6.4-1\bin"
+
+[Environment]::SetEnvironmentVariable("PATH", "$path", "Machine")
+
+# b. 64-bit
+
+iex "$curl -LO http://win32builder.gnome.org/packages/3.6/gtk+_3.6.4-1_win64.zip"
+
+unzip gtk+_3.6.4-1_win64.zip -d c:\gtk+_3.6.4-1_64bit
+
+$path += ";C:\gtk+_3.6.4-1_64bit\bin"
+
+[Environment]::SetEnvironmentVariable("PATH", "$path", "Machine")
+
+# gtkmm
+
+iex "$curl -LO http://s3.amazonaws.com/bioc-windows-setup/gtkmm.zip"
+
+unzip gtkmm.zip -d c:\gtkmm
+
+$path += ";c:\gtkmm\i386\bin;c:\gtkmm\x64\bin"
+
+[Environment]::SetEnvironmentVariable("PATH", "$path", "Machine")
