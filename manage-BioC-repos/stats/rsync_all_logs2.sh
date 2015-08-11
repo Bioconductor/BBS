6dca54953a0be81330ba8b3d248017373cace44e
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-08-11 19:40:39 +0000

    pull from master, not krait
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/BBS@107327 bc3139a8-67e5-0310-9ffc-ced21a209358

--- manage-BioC-repos/stats/rsync_all_logs2.sh
+++ manage-BioC-repos/stats/rsync_all_logs2.sh
@@ -28,5 +28,5 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "> RETRIEVING APACHE2 LOG FILES FROM $LOGS_REMOTE_PATH/krait"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo ""
-rsync -ave ssh "$LOGS_REMOTE_PATH/krait/apache2/bioconductor-access.log-*" $LOGS_LOCAL_PATH/apache2/krait
+rsync -ave ssh "ubuntu@master.bioconductor.org:/apache2/bioconductor-access.log-*" $LOGS_LOCAL_PATH/apache2/krait
 
