2c5541095c8bc7ed9fdbaaa87ed415d8d3a08108
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-08-12 23:31:18 +0000

    pull master logs from rhino01
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioC/admin/build/BBS@107360 bc3139a8-67e5-0310-9ffc-ced21a209358

--- manage-BioC-repos/stats/rsync_all_logs2.sh
+++ manage-BioC-repos/stats/rsync_all_logs2.sh
@@ -25,8 +25,9 @@ rsync -ave ssh "$LOGS_REMOTE_PATH/mamba/squid/access.log*" $LOGS_LOCAL_PATH/squi
 
 echo ""
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo "> RETRIEVING APACHE2 LOG FILES FROM master.bioconductor.org..."
+echo "> RETRIEVING APACHE2 LOG FILES FROM $LOGS_REMOTE_PATH/master"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo ""
 rsync -ave ssh "ubuntu@master.bioconductor.org:/var/log/apache2/bioconductor-access.log-*" $LOGS_LOCAL_PATH/apache2/krait
+rsync -ave ssh "$LOGS_REMOTE_PATH/master/bioconductor-access.log-*" $LOGS_LOCAL_PATH/apache2/master
 
