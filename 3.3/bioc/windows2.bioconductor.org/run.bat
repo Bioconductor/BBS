280fc98933c711d61b904e45ad3d4bc8d50e57be
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-09-09 20:38:02 -0700

    replace e:\ with c:\

--- 3.3/bioc/windows2.bioconductor.org/run.bat
+++ 3.3/bioc/windows2.bioconductor.org/run.bat
@@ -8,7 +8,7 @@ call config.bat
 
 call %BBS_HOME%\utils\clean-before-run.bat
 
-set YYYYMMDD=%datc:~10,4%%datc:~4,2%%datc:~7,2%
+set YYYYMMDD=%date:~10,4%%date:~4,2%%date:~7,2%
 set LOG_FILE=%BBS_WORK_TOPDIR%\log\%BBS_NODE_HOSTNAME%-run-%YYYYMMDD%.log
 
 %BBS_HOME%\BBS-run.py >>%LOG_FILE% 2>&1
