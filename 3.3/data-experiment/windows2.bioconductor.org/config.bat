04e8d1b2e087a103c9193baa409a894fe29533ea
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-09-09 20:36:04 -0700

    replace e:\ with c:\

--- 3.3/data-experiment/windows2.bioconductor.org/config.bat
+++ 3.3/data-experiment/windows2.bioconductor.org/config.bat
@@ -7,9 +7,9 @@ set BBS_DEBUG=0
 
 set BBS_NODE_HOSTNAME=windows2.bioconductor.org
 set BBS_USER=biocbuild
-set BBS_RSAKEY=E:\biocbld\.BBS\id_rsa
-set BBS_WORK_TOPDIR=E:\biocbld\bbs-3.3-data-experiment
-set BBS_R_HOME=E:\biocbld\bbs-3.3-bioc\R
+set BBS_RSAKEY=c:\biocbld\.BBS\id_rsa
+set BBS_WORK_TOPDIR=c:\biocbld\bbs-3.3-data-experiment
+set BBS_R_HOME=c:\biocbld\bbs-3.3-bioc\R
 set BBS_NB_CPU=6
 
 set BBS_STAGE2_MODE=multiarch
