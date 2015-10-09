15f93ec14ccb1e7887e67068f7297fee2d356f21
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-10-09 14:35:27 -0700

    add morelia to data/exp builds

--- 3.3/data-experiment/linux2.bioconductor.org/config.sh
+++ 3.3/data-experiment/linux2.bioconductor.org/config.sh
@@ -30,8 +30,7 @@ cd "$wd0"
 # Needed only on the node performing stage6 (must be run on the
 # BBS_CENTRAL_RHOST machine).
 
-# FIXME add mac.mavericks.binary:morelia/buildbin
-export BBS_OUTGOING_MAP="source:linux2.bioconductor.org/buildsrc win.binary:windows2.bioconductor.org/buildbin mac.binary:petty/buildbin"
+export BBS_OUTGOING_MAP="source:linux2.bioconductor.org/buildsrc win.binary:windows2.bioconductor.org/buildbin mac.binary:morelia/buildbin"
 
 # Needed only on the node performing stage7a (BBS-make-STATUS_DB.py) and
 # stage8 (BBS-report.py)
@@ -40,7 +39,7 @@ export BBS_OUTGOING_MAP="source:linux2.bioconductor.org/buildsrc win.binary:wind
 # must be run on the BBS_CENTRAL_RHOST machine.
 
 # FIXME add morelia:bin
-export BBS_REPORT_NODES="linux2.bioconductor.org windows2.bioconductor.org:bin petty:bin"
+export BBS_REPORT_NODES="linux2.bioconductor.org windows2.bioconductor.org:bin morelia:bin"
 export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
 export BBS_REPORT_CSS="$BBS_HOME/$BBS_BIOC_VERSION/report.css"
 export BBS_REPORT_BGIMG="$BBS_HOME/images/DEVEL3b-green2.png"
