7394f90052118b3b58810ee3d42d5e256b88055a
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-10-09 13:23:37 -0700

    perceval now has more cores

--- 3.2/data-experiment/perceval/config.sh
+++ 3.2/data-experiment/perceval/config.sh
@@ -14,7 +14,7 @@ export BBS_USER="biocbuild"
 export BBS_RSAKEY="/Users/biocbuild/.BBS/id_rsa"
 export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.2-data-experiment"
 export BBS_R_HOME="/Library/Frameworks/R.framework/Versions/Current/Resources"
-export BBS_NB_CPU=7
+export BBS_NB_CPU=8
 
 
 
