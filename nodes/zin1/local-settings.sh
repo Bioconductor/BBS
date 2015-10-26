ff0bd47147510d35735a3694a032743d44a3862f
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-10-26 12:00:25 -0700

    add another comment

--- nodes/zin1/local-settings.sh
+++ nodes/zin1/local-settings.sh
@@ -5,6 +5,8 @@
 
 # FIXME - parameterize this as much as possible
 # maybe use 'which' to find locations of programs
+# but note that 'which' relies on the PATH which
+# may be set incorrectly in crontab.
 
 if [ "$BBS_HOME" == "" ]; then
     export BBS_HOME="/home/biocbuild/BBS"
