bf831c6f5cfd854cc00bf0f31280c3a038c332af
Dan Tenenbaum <dtenenba@fhcrc.org>
2016-04-29 11:51:17 -0700

    set up to run 3.4 builds on zin1

---
+++ manage-BioC-repos/3.4/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ==============================================================
+# Settings shared by all scripts in manage-BioC-repos/3.4 folder
+# ==============================================================
+
+BIOC_VERSION="3.4"
+R_VERSION="3.4"
+R="$HOME/bin/R-$R_VERSION"
+BACKGROUND_COLOR="#E0C266"
+
