25e021390641e719429d9c0558fea56eb26d2fe5
Hervé Pagès <hpages@fredhutch.org>
2016-09-21 10:40:29 -0700

    push report to standard location on the website

--- 3.4-RPCI/data-experiment/malbec1/config.sh
+++ 3.4-RPCI/data-experiment/malbec1/config.sh
@@ -47,5 +47,5 @@ export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
 export BBS_REPORT_CSS="$BBS_HOME/${BBS_BIOC_VERSION}-RPCI/report.css"
 export BBS_REPORT_BGIMG="$BBS_HOME/images/DEVEL3b.png"
 export BBS_REPORT_JS="$BBS_HOME/${BBS_BIOC_VERSION}-RPCI/report.js"
-export BBS_REPORT_DEST_DIR="webadmin@master.bioconductor.org:/extra/www/bioc/checkResults/$BBS_BIOC_VERSION/RPCI/data-experiment-LATEST"
+export BBS_REPORT_DEST_DIR="webadmin@master.bioconductor.org:/extra/www/bioc/checkResults/$BBS_BIOC_VERSION/data-experiment-LATEST"
 
