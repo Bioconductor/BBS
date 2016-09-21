4aed15632d04515739a532d76517a2449b885d59
Hervé Pagès <hpages@fredhutch.org>
2016-09-21 10:49:32 -0700

    one more tweak to malbec1/config.sh

--- 3.4-RPCI/bioc/malbec1/config.sh
+++ 3.4-RPCI/bioc/malbec1/config.sh
@@ -50,7 +50,7 @@ cd "$wd0"
 # the dropped node is back.
 
 #export BBS_OUTGOING_MAP="source:malbec1/buildsrc win.binary:tokay1/buildbin mac.binary.mavericks:morelia/buildbin"
-export BBS_OUTGOING_MAP="source:malbec1/buildsrc"
+export BBS_OUTGOING_MAP="source:malbec1/buildsrc win.binary:tokay1/buildbin"
 
 # Needed only on the node performing stage7a (BBS-make-STATUS_DB.py) and
 # stage8 (BBS-report.py)
@@ -59,7 +59,7 @@ export BBS_OUTGOING_MAP="source:malbec1/buildsrc"
 # must be run on the BBS_CENTRAL_RHOST machine.
 
 #export BBS_REPORT_NODES="malbec1 tokay1:bin morelia:bin"
-export BBS_REPORT_NODES="malbec1"
+export BBS_REPORT_NODES="malbec1 tokay1:bin"
 #export BBS_SVNCHANGELOG_URL="http://fgc.lsi.umich.edu/cgi-bin/blosxom.cgi"
 export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
 export BBS_REPORT_CSS="$BBS_HOME/${BBS_BIOC_VERSION}-RPCI/report.css"
