2a1bc26de0ae7df6a274e4026f8dfa351a86c86f
Hervé Pagès <hpages@fredhutch.org>
2016-10-31 13:52:10 -0700

    clean and align pushRepos-*.sh scripts for BioC 3.5

--- manage-BioC-repos/3.5/pushRepos-data-annotation.sh
+++ manage-BioC-repos/3.5/pushRepos-data-annotation.sh
@@ -4,11 +4,9 @@ cd "$HOME/manage-BioC-repos/3.5"
 
 . ./config.sh
 
-REPOS_ROOT="$HOME/PACKAGES/$BIOC_VERSION/data/annotation"
+REPOS_SUBDIR="data/annotation"
+REPOS_ROOT="$HOME/PACKAGES/$BIOC_VERSION/$REPOS_SUBDIR"
+DEST="webadmin@master.bioconductor.org:/extra/www/bioc/packages/$BIOC_VERSION/$REPOS_SUBDIR"
 
-
-DEST="webadmin@master.bioconductor.org:/extra/www/bioc/packages/$BIOC_VERSION/data/annotation"
-
-
-rsync --delete -ave ssh $REPOS_ROOT/bin $REPOS_ROOT/manuals $REPOS_ROOT/REPOSITORY $REPOS_ROOT/src $REPOS_ROOT/SYMBOLS $REPOS_ROOT/VIEWS $REPOS_ROOT/vignettes $REPOS_ROOT/licenses $REPOS_ROOT/citations $DEST
+rsync --delete -ave ssh $REPOS_ROOT/src $REPOS_ROOT/bin $REPOS_ROOT/manuals $REPOS_ROOT/REPOSITORY $REPOS_ROOT/SYMBOLS $REPOS_ROOT/VIEWS $REPOS_ROOT/vignettes $REPOS_ROOT/citations $REPOS_ROOT/news $REPOS_ROOT/licenses $REPOS_ROOT/readmes $REPOS_ROOT/install $DEST
 
