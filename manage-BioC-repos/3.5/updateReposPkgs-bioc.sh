48dedda4d99ed8877b7b6dfd4feb9899a4bb072e
Hervé Pagès <hpages@fredhutch.org>
2017-04-18 05:09:29 -0700

    completely stop propagating any Mavericks binary package for BioC >= 3.5

--- manage-BioC-repos/3.5/updateReposPkgs-bioc.sh
+++ manage-BioC-repos/3.5/updateReposPkgs-bioc.sh
@@ -12,7 +12,6 @@ PROPAGATION_DB_FILE="$BBS_OUTGOING_DIR/../PROPAGATE_STATUS_DB.txt"
 REPOS_ROOT="$HOME/PACKAGES/$BIOC_VERSION/bioc"
 SRC_CONTRIB="$REPOS_ROOT/src/contrib"
 WIN_CONTRIB="$REPOS_ROOT/bin/windows/contrib/$R_VERSION"
-MAC_MAVERICKS_CONTRIB="$REPOS_ROOT/bin/macosx/mavericks/contrib/$R_VERSION"
 MAC_ELCAPITAN_CONTRIB="$REPOS_ROOT/bin/macosx/el-capitan/contrib/$R_VERSION"
 
 update_repo()
@@ -41,10 +40,6 @@ update_repo "$SRC_CONTRIB" "source" "tar.gz"
 echo ""
 echo "Updating $BIOC_VERSION/bioc repo with Windows binary packages..."
 update_repo "$WIN_CONTRIB" "win.binary" "zip"
-
-echo ""
-echo "Updating $BIOC_VERSION/bioc repo with Mac Mavericks binary packages..."
-update_repo "$MAC_MAVERICKS_CONTRIB" "mac.binary.mavericks" "tgz"
 
 echo ""
 echo "Updating $BIOC_VERSION/bioc repo with Mac El Capitan binary packages..."
