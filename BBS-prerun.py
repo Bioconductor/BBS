c0a92a18a4ce2de686804b62733f13222be99c66
aoles <andrzej.oles@gmail.com>
2017-06-07 12:01:05 -0400

    Fix typo

--- BBS-prerun.py
+++ BBS-prerun.py
@@ -174,7 +174,7 @@ def snapshotMEAT0git(MEAT0_path):
                 cmd = '%s -C %s clone https://git.bioconductor.org/packages/%s' % (git_cmd, MEAT0_path, pkg)
             bbs.jobs.doOrDie(cmd)
             ## checkout based on date, see https://stackoverflow.com/a/6990682/2792099            
-            cmd = '%s merge `%s rev-list -n 1 --before="%s" master`' %s (git_cmd, git_cmd, snapshot_date)
+            cmd = '%s merge `%s rev-list -n 1 --before="%s" master`' % (git_cmd, git_cmd, snapshot_date)
             bbs.jobs.doOrDie(cmd)
     return snapshot_date
 
