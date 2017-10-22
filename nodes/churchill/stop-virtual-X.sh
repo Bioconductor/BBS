f03e5e70ea13a104f76fc60190cacd3ca8035fe5
Hervé Pagès <hpages@fredhutch.org>
2017-10-22 11:41:50 -0700

    remove a bunch of old build nodes from the node db

--- nodes/churchill/stop-virtual-X.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# =======================================================
-# Stop the virtual X server started by start-virtual-X.sh
-# =======================================================
-
-kill $xvfb_pid
-pkill Xsun
