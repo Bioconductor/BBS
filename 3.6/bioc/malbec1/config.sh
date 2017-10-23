dddb631708468f2527a8eee997b5383fc92f1077
Hervé Pagès <hpages@fredhutch.org>
2017-10-23 09:02:45 -0700

    cosmetic

--- 3.6/bioc/malbec1/config.sh
+++ 3.6/bioc/malbec1/config.sh
@@ -14,7 +14,7 @@ export BBS_USER="biocbuild"
 export BBS_RSAKEY="/home/biocbuild/.BBS/id_rsa"
 export BBS_WORK_TOPDIR="/home/biocbuild/bbs-3.6-bioc"
 export BBS_R_HOME="$BBS_WORK_TOPDIR/R"
-export BBS_NB_CPU=10
+export BBS_NB_CPU=10  # 20 cores are available
 
 
 
