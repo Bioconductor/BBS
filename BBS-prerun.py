Bioconductor/BBS

commit 685479164011bb36ca4f62504346143d0a1d812f

Support 2 manifest file formats: plain text (1 package name per line) and DCF-like (lines prefixed with 'Package:').

BBS-prerun.py
@@ -188,9 +188,7 @@ def update_git_MEAT0(MEAT0_path=None, snapshot_date=None,
                              manifest_git_branch,
                              1)
     ## iterate over manifest to update pkg dirs
-    dcf = open(manifest_path, 'r')
-    pkgs = bbs.parse.readPkgsFromDCF(dcf)
-    dcf.close()
+    pkgs = bbs.parse.read_manifest(manifest_path)
     i = 0
     for pkg in pkgs:
         i = i + 1
@@ -232,9 +230,7 @@ def writeAndUploadMeatInfo(work_topdir):
     snapshot_date = snapshotMEAT0(MEAT0_path)
     manifest_path = BBSvars.manifest_path
     print "BBS> [writeAndUploadMeatInfo] Get pkg list from %s" % manifest_path
-    dcf = open(manifest_path, 'r')
-    pkgs = bbs.parse.readPkgsFromDCF(dcf)
-    dcf.close()
+    pkgs = bbs.parse.read_manifest(manifest_path)
     writeAndUploadMeatIndex(pkgs, MEAT0_path)
     writeAndUploadVcsMeta(snapshot_date)
     return

bbs/parse.py
@@ -15,6 +15,30 @@ import subprocess
 import BBScorevars
 from bbs.dcf.dcfrecordsparser import DcfRecordParser
 
+### Trim 'Package:' prefix from every input line so will accept a plain text
+### file with 1 package name per line, whether the line is prefixed with
+### 'Package:' or not. Raise an exception if the file contains DCF-like lines
+### for fields other than 'Package:'.
+def read_manifest(manifest_path):
+    f = open(manifest_path, 'r')
+    pkgs = []
+    lineno = 0
+    for pkg in f:
+        lineno += 1
+        ## Skip blank and comment lines.
+        if pkg.strip() == '' or pkg.startswith('#'):
+            continue
+        ## Strip the 'Package:' prefix.
+        if pkg.startswith('Package:'):
+            pkg = pkg[len('Package:'):]
+        elif pkg.find(':') >= 0:
+            f.close()
+            raise Exception('invalid line %d in file \'%s\':\n\n%s' % \
+                            (lineno, manifest_path, pkg))
+        pkgs.append(pkg.strip())
+    f.close()
+    return pkgs
+
 class DcfFieldNotFoundError(Exception):
     def __init__(self, file, field):
         self.file = file

utils/update_bioc_git_repos.py
@@ -4,6 +4,7 @@
 import sys
 import os
 
+import parse
 import git
 
 home = os.path.expanduser('~')
@@ -27,17 +28,6 @@ def update_packages(pkg_dir, pkgs, git_branch=None, skip=None):
         git.update_git_clone(pkg_git_clone, pkg_git_repo_url, git_branch)
     return
 
-def read_manifest(manifest_path):
-    dcf = open(manifest_path, 'r')
-    pkgs = []
-    for line in dcf:
-        if not line.startswith('Package:'):
-            continue
-        pkg = line[len('Package:'):].strip()
-        pkgs.append(pkg)
-    dcf.close()
-    return pkgs
-
 def update_packages_in_current_working_dir(git_branch=None, skip=None):
     key = 'MANIFEST_FILE'
     print 'BBS> Environment variable %s is' % key,
@@ -46,7 +36,7 @@ def update_packages_in_current_working_dir(git_branch=None, skip=None):
         print 'defined and set to:'
         print 'BBS>     %s' % manifest_path
         print 'BBS> ==> Using %s as manifest file...' % manifest_path
-        pkgs = read_manifest(manifest_path)
+        pkgs = parse.read_manifest(manifest_path)
         print 'BBS> Nb of packages listed in manifest file: %d' % len(pkgs)
     else:
         print 'NOT defined (or is set to '
@@ -73,7 +63,7 @@ def update_packages_from_manifest(pkg_dir, manifest_file,
                                   git_branch=None, skip=None):
     update_manifest(git_branch)
     manifest_path = os.path.join(manifest_git_clone, manifest_file)
-    pkgs = read_manifest(manifest_path)
+    pkgs = parse.read_manifest(manifest_path)
     update_packages(pkg_dir, pkgs, git_branch, skip)
     return
 

commit f370466c412baaf0badf83ccfbd6e8cfaef25d12

fix issue introduced by commit 685479164011bb36ca4f62504346143d0a1d812f

BBS-prerun.py
@@ -13,6 +13,7 @@ import shutil
 import time
 
 import bbs.fileutils
+import bbs.manifest
 import bbs.parse
 import bbs.dcf
 import bbs.jobs
@@ -188,7 +189,7 @@ def update_git_MEAT0(MEAT0_path=None, snapshot_date=None,
                              manifest_git_branch,
                              1)
     ## iterate over manifest to update pkg dirs
-    pkgs = bbs.parse.read_manifest(manifest_path)
+    pkgs = bbs.manifest.read(manifest_path)
     i = 0
     for pkg in pkgs:
         i = i + 1
@@ -230,7 +231,7 @@ def writeAndUploadMeatInfo(work_topdir):
     snapshot_date = snapshotMEAT0(MEAT0_path)
     manifest_path = BBSvars.manifest_path
     print "BBS> [writeAndUploadMeatInfo] Get pkg list from %s" % manifest_path
-    pkgs = bbs.parse.read_manifest(manifest_path)
+    pkgs = bbs.manifest.read(manifest_path)
     writeAndUploadMeatIndex(pkgs, MEAT0_path)
     writeAndUploadVcsMeta(snapshot_date)
     return

bbs/manifest.py
@@ -1,0 +1,35 @@
+#!/usr/bin/env python
+##############################################################################
+###
+### This file is part of the BBS software (Bioconductor Build System).
+###
+### Author: Herve Pages (hpages@fhcrc.org)
+### Last modification: Oct 24, 2017
+###
+### manifest module
+###
+
+### Trim 'Package:' prefix from every input line so will accept a plain text
+### file with 1 package name per line, whether the line is prefixed with
+### 'Package:' or not. Raise an exception if the file contains DCF-like lines
+### for fields other than 'Package:'.
+def read(manifest_path):
+    f = open(manifest_path, 'r')
+    pkgs = []
+    lineno = 0
+    for pkg in f:
+        lineno += 1
+        ## Skip blank and comment lines.
+        if pkg.strip() == '' or pkg.startswith('#'):
+            continue
+        ## Strip the 'Package:' prefix.
+        if pkg.startswith('Package:'):
+            pkg = pkg[len('Package:'):]
+        elif pkg.find(':') >= 0:
+            f.close()
+            raise Exception('invalid line %d in file \'%s\':\n\n%s' % \
+                            (lineno, manifest_path, pkg))
+        pkgs.append(pkg.strip())
+    f.close()
+    return pkgs
+

bbs/parse.py
@@ -15,30 +15,6 @@ import subprocess
 import BBScorevars
 from bbs.dcf.dcfrecordsparser import DcfRecordParser
 
-### Trim 'Package:' prefix from every input line so will accept a plain text
-### file with 1 package name per line, whether the line is prefixed with
-### 'Package:' or not. Raise an exception if the file contains DCF-like lines
-### for fields other than 'Package:'.
-def read_manifest(manifest_path):
-    f = open(manifest_path, 'r')
-    pkgs = []
-    lineno = 0
-    for pkg in f:
-        lineno += 1
-        ## Skip blank and comment lines.
-        if pkg.strip() == '' or pkg.startswith('#'):
-            continue
-        ## Strip the 'Package:' prefix.
-        if pkg.startswith('Package:'):
-            pkg = pkg[len('Package:'):]
-        elif pkg.find(':') >= 0:
-            f.close()
-            raise Exception('invalid line %d in file \'%s\':\n\n%s' % \
-                            (lineno, manifest_path, pkg))
-        pkgs.append(pkg.strip())
-    f.close()
-    return pkgs
-
 class DcfFieldNotFoundError(Exception):
     def __init__(self, file, field):
         self.file = file

utils/update_bioc_git_repos.py
@@ -4,7 +4,7 @@
 import sys
 import os
 
-import parse
+import manifest
 import git
 
 home = os.path.expanduser('~')
@@ -36,7 +36,7 @@ def update_packages_in_current_working_dir(git_branch=None, skip=None):
         print 'defined and set to:'
         print 'BBS>     %s' % manifest_path
         print 'BBS> ==> Using %s as manifest file...' % manifest_path
-        pkgs = parse.read_manifest(manifest_path)
+        pkgs = manifest.read(manifest_path)
         print 'BBS> Nb of packages listed in manifest file: %d' % len(pkgs)
     else:
         print 'NOT defined (or is set to '
@@ -63,7 +63,7 @@ def update_packages_from_manifest(pkg_dir, manifest_file,
                                   git_branch=None, skip=None):
     update_manifest(git_branch)
     manifest_path = os.path.join(manifest_git_clone, manifest_file)
-    pkgs = parse.read_manifest(manifest_path)
+    pkgs = manifest.read(manifest_path)
     update_packages(pkg_dir, pkgs, git_branch, skip)
     return
 