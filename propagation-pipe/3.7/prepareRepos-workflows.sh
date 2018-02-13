a93fea1e3117a6706115efc6175aedb29b96d050
aoles <andrzej.oles@gmail.com>
2018-02-13 08:45:31 -0500

    Add propagation scripts for workflows

---
+++ propagation-pipe/3.7/prepareRepos-workflows.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd "$HOME/propagation-pipe/3.7"
+
+. ./config.sh
+. ./prepareRepos-workflows.config
+. ./prepareRepos.sh
+
---
+++ propagation-pipe/3.7/pushRepos-workflows.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd "$HOME/propagation-pipe/3.7"
+
+. ./config.sh
+
+REPOS_SUBDIR="workflows"
+REPOS_ROOT="$HOME/PACKAGES/$BIOC_VERSION/$REPOS_SUBDIR"
+DEST="webadmin@master.bioconductor.org:/extra/www/bioc/packages/$BIOC_VERSION/$REPOS_SUBDIR"
+
+rsync --delete -ave ssh $REPOS_ROOT/src $REPOS_ROOT/bin $REPOS_ROOT/webvigs $REPOS_ROOT/REPOSITORY $REPOS_ROOT/SYMBOLS $REPOS_ROOT/VIEWS $REPOS_ROOT/vignettes $REPOS_ROOT/citations $REPOS_ROOT/news $REPOS_ROOT/licenses $REPOS_ROOT/readmes $REPOS_ROOT/install $DEST
+
