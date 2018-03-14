487dd684c43fa77f80616dce4d2ecaa156803854
Hervé Pagès <hpages@fredhutch.org>
2018-03-13 20:48:40 -0700

    Back to using BBS_NB_CPU=24 for the software builds on tokay2
    Even though tokay2 has 20 cores (40 logical cpus), using BBS_NB_CPU=30
    seems to be counter-productive.

--- 3.7/bioc/tokay2/config.bat
+++ 3.7/bioc/tokay2/config.bat
@@ -10,7 +10,7 @@ set BBS_USER=biocbuild
 set BBS_RSAKEY=C:\Users\biocbuild\.BBS\id_rsa
 set BBS_WORK_TOPDIR=C:\Users\biocbuild\bbs-3.7-bioc
 set BBS_R_HOME=%BBS_WORK_TOPDIR%\R
-set BBS_NB_CPU=30
+set BBS_NB_CPU=24
 
 set BBS_STAGE2_MODE=multiarch
 set BBS_STAGE4_MODE=multiarch
