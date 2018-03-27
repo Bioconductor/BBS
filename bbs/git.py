04808697240f5c7be2b50e7af1731e40dd52f160
Hervé Pagès <hpages@fredhutch.org>
2018-03-27 15:16:49 -0700

    improve output a little

--- bbs/git.py
+++ bbs/git.py
@@ -89,10 +89,11 @@ def update_git_clone(clone_path, repo_url, branch=None, depth=None, snapshot_dat
         retcode = _update_clone(clone_path, repo_url, branch, snapshot_date)
         if retcode == 0:
             return
+        print ""
         print "bbs.git.update_git_clone> _update_clone() failed " + \
               "with error code %d!" % retcode
         print "bbs.git.update_git_clone> --> will try to re-create " + \
-              "the git clone from scratch ..."
+              "git clone from scratch ..."
         print ""
         print "bbs.git.update_git_clone> rm -r %s" % clone_path
         fileutils.nuke_tree(clone_path)
