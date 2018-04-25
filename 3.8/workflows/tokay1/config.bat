6efcb155deaf73a03d28d6f887c997c1f7656fd3
vobencha <valerie.obenchain@roswellpark.org>
2018-04-25 12:36:19 -0700

    add Mac and Windows to workflow builds for 3.7 and 3.8

--- 3.8/workflows/tokay1/config.bat
+++ 3.8/workflows/tokay1/config.bat
@@ -1,15 +1,15 @@
 @rem ===================
-@rem Settings for tokay2
+@rem Settings for tokay1
 @rem ===================
 
 
 set BBS_DEBUG=0
 
-set BBS_NODE_HOSTNAME=tokay2
+set BBS_NODE_HOSTNAME=tokay1
 set BBS_USER=biocbuild
 set BBS_RSAKEY=C:\Users\biocbuild\.BBS\id_rsa
-set BBS_WORK_TOPDIR=C:\Users\biocbuild\bbs-3.7-data-experiment
-set BBS_R_HOME=C:\Users\biocbuild\bbs-3.7-bioc\R
+set BBS_WORK_TOPDIR=C:\Users\biocbuild\bbs-3.8-workflows
+set BBS_R_HOME=C:\Users\biocbuild\bbs-3.8-bioc\R
 set BBS_NB_CPU=15
 set BBS_CHECK_NB_CPU=24
 
