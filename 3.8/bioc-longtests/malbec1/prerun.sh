fd057cabcbe195a956699d93d5c6225d2804366a
Hervé Pagès <hpages@fredhutch.org>
2018-09-18 16:41:14 -0700

    Tweak to bioc-longtests builds
    Make sure the prerun.sh and run.sh scripts exit immediately if a Python
    script exits with a non-zero status.

--- 3.8/bioc-longtests/malbec1/prerun.sh
+++ 3.8/bioc-longtests/malbec1/prerun.sh
@@ -2,5 +2,7 @@
 
 . ./config.sh
 
+set -e # Exit immediately if a simple command exits with a non-zero status.
+
 $BBS_HOME/BBS-prerun.py make-central-rdir
 $BBS_HOME/BBS-prerun.py upload-meat-info
--- 3.8/bioc-longtests/malbec1/run.sh
+++ 3.8/bioc-longtests/malbec1/run.sh
@@ -2,5 +2,7 @@
 
 . ./config.sh
 
+set -e # Exit immediately if a simple command exits with a non-zero status.
+
 $BBS_HOME/BBS-run.py STAGE3
 $BBS_HOME/BBS-run.py STAGE4
