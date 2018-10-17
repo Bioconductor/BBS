4b475f22f3ecedbbb971a7beae97c3b03775c09b
vobencha <valerie.obenchain@roswellpark.org>
2018-10-17 09:20:40 -0700

    Add scripts for 3.9 propagation pipe

---
+++ propagation-pipe/3.9/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# =============================================================
+# Settings shared by all scripts in propagation-pipe/3.9 folder
+# =============================================================
+
+BIOC_VERSION="3.9"
+R_VERSION="3.5"
+R="$HOME/bin/R-$R_VERSION"
+BACKGROUND_COLOR="#B88864"
---
+++ propagation-pipe/3.9/prepareRepos-data-annotation.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd "$HOME/propagation-pipe/3.9"
+
+. ./config.sh
+. ./prepareRepos-data-annotation.config
+. ./prepareRepos.sh
+
