3591447f5a6e9d8df0167804c5e5bc5cd85c1243
Hervé Pagès <hpages@fredhutch.org>
2018-12-14 14:17:21 -0800

    R-fix-flags.sh now also adds -Wall to CXX[98|11|14]FLAGS

--- utils/R-fix-flags.sh
+++ utils/R-fix-flags.sh
@@ -5,6 +5,9 @@ set -e  # Exit immediately if a simple command exits with a non-zero status
 new_flag="-Wall"
 cflags_line="^CFLAGS *=.*"
 cxxflags_line="^CXXFLAGS *=.*"
+cxx98flags_line="^CXX98FLAGS *=.*"
+cxx11flags_line="^CXX11FLAGS *=.*"
+cxx14flags_line="^CXX14FLAGS *=.*"
 fflags_line="^FFLAGS *=.*"
 
 mv -i Makeconf Makeconf.original
@@ -14,6 +17,12 @@ cat Makeconf.original \
 	| sed -r "s/^$cflags_line/\\0 $new_flag/" \
 	| sed -r "s/^($cxxflags_line) $new_flag +(.*)$/\\1 \\2/" \
 	| sed -r "s/^$cxxflags_line/\\0 $new_flag/" \
+	| sed -r "s/^($cxx98flags_line) $new_flag +(.*)$/\\1 \\2/" \
+	| sed -r "s/^$cxx98flags_line/\\0 $new_flag/" \
+	| sed -r "s/^($cxx11flags_line) $new_flag +(.*)$/\\1 \\2/" \
+	| sed -r "s/^$cxx11flags_line/\\0 $new_flag/" \
+	| sed -r "s/^($cxx14flags_line) $new_flag +(.*)$/\\1 \\2/" \
+	| sed -r "s/^$cxx14flags_line/\\0 $new_flag/" \
 	| sed -r "s/^($fflags_line) $new_flag +(.*)$/\\1 \\2/" \
 	| sed -r "s/^$fflags_line/\\0 $new_flag/" > Makeconf
 
