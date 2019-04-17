4c3303f96058d02f1711173ff63d026bd4cc8b71
Hervé Pagès <hpages@fredhutch.org>
2019-04-17 09:27:05 -0700

    set BBS_NB_CPU to 8 instead of 10 (on Linux) for the workflows builds

--- 3.9/workflows/malbec2/config.sh
+++ 3.9/workflows/malbec2/config.sh
@@ -14,7 +14,7 @@ export BBS_USER="biocbuild"
 export BBS_RSAKEY="/home/biocbuild/.BBS/id_rsa"
 export BBS_WORK_TOPDIR="/home/biocbuild/bbs-3.9-workflows"
 export BBS_R_HOME="/home/biocbuild/bbs-3.9-bioc/R"
-export BBS_NB_CPU=10  # 20 cores are available
+export BBS_NB_CPU=8  # 20 cores are available
 
 
 
