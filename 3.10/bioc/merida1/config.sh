8dac254d90fabde075db9a7b5ad731abb2db79a1
Hervé Pagès <hpages@fredhutch.org>
2019-04-28 13:45:20 -0700

    Set BBS_NB_CPU on merida1 & celaya2 to 14 instead of 12 for software builds

--- 3.10/bioc/merida1/config.sh
+++ 3.10/bioc/merida1/config.sh
@@ -14,7 +14,7 @@ export BBS_USER="biocbuild"
 export BBS_RSAKEY="/Users/biocbuild/.BBS/id_rsa"
 export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.10-bioc"
 export BBS_R_HOME="/Library/Frameworks/R.framework/Versions/Current/Resources"
-export BBS_NB_CPU=12        # 24 cores are available
+export BBS_NB_CPU=14        # 24 cores are available
 export BBS_CHECK_NB_CPU=18  # 24 cores are available
 
 
