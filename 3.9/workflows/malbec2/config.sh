92724d374cbe9bb9c8796d97466e78c6955cea27
Hervé Pagès <hpages@fredhutch.org>
2019-04-29 09:57:00 -0700

    Tweak numer of cpus used on malbec1 & malbec2 during workflows builds
    Set BBS_NB_CPU to 6 instead of 8.
    Set BBS_CHECK_NB_CPU to 8 (was not set until now).

--- 3.9/workflows/malbec2/config.sh
+++ 3.9/workflows/malbec2/config.sh
@@ -14,7 +14,8 @@ export BBS_USER="biocbuild"
 export BBS_RSAKEY="/home/biocbuild/.BBS/id_rsa"
 export BBS_WORK_TOPDIR="/home/biocbuild/bbs-3.9-workflows"
 export BBS_R_HOME="/home/biocbuild/bbs-3.9-bioc/R"
-export BBS_NB_CPU=8  # 20 cores are available
+export BBS_NB_CPU=6        # 20 cores are available
+export BBS_CHECK_NB_CPU=8  # 20 cores are available
 
 
 
