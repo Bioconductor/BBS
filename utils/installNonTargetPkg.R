13c49b0f9738171d1f07deaf345622c60862735f
Hervé Pagès <hpages@fredhutch.org>
2019-04-29 12:54:07 -0700

    Make sure installNonTargetPkg() always tries to install the binary first on Windows and Mac

--- utils/installNonTargetPkg.R
+++ utils/installNonTargetPkg.R
@@ -14,17 +14,34 @@
 
 installNonTargetPkg <- function(pkg, multiArch=FALSE)
 {
-    type <- getOption("pkgType")
-    if (type != "source")
-        type <- "both"
-
+    ## On Windows and Mac we always try to install the binary first (even
+    ## when the source version is later). Only if it fails, we try to install
+    ## the source.
+    if (getOption("pkgType") != "source") {
+        ## Set 'type' to '.Platform$pkgType' to prevent install.packages()
+        ## from setting it to 'getOption("pkgType")' (which is "both" by
+        ## default on Windows and Mac).
+        install.packages(pkg, repos=.get_non_target_repos(),
+                              dependencies=FALSE,
+                              type=.Platform$pkgType)
+        if (pkg %in% rownames(installed.packages()))
+            return(invisible(NULL))
+        message("")
+        message("------------------------------------------------------------")
+        message("Installation of binary failed! ",
+                "Now trying to install the source ...")
+        message("------------------------------------------------------------")
+        message("")
+    }
+    ## Try to install the source.
     INSTALL_opts <- ""
     if (multiArch)
         INSTALL_opts <- "--merge-multiarch"
-    if(grepl("^x86_64-apple-darwin", R.Version()$platform))
+    if (grepl("^x86_64-apple-darwin", R.Version()$platform))
         INSTALL_opts <- "--no-multiarch --no-test-load"
-
-    install.packages(pkg, repos=.get_non_target_repos(), dependencies=FALSE,
-                          type=type, INSTALL_opts=INSTALL_opts)
+    install.packages(pkg, repos=.get_non_target_repos(),
+                          dependencies=FALSE,
+                          type="source",
+                          INSTALL_opts=INSTALL_opts)
 }
 
