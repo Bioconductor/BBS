88bfa1f81717bfbb6eae30782780996ea7ed2629
Hervé Pagès <hpages@fredhutch.org>
2019-12-19 08:27:36 -0800

    Disable _R_CHECK_LENGTH_1_LOGIC2_ setting for now

--- 3.11/R_env_vars.sh
+++ 3.11/R_env_vars.sh
@@ -13,7 +13,8 @@
 export _R_CHECK_EXECUTABLES_=false
 export _R_CHECK_EXECUTABLES_EXCLUSIONS_=false
 export _R_CHECK_LENGTH_1_CONDITION_=package:_R_CHECK_PACKAGE_NAME_,abort,verbose
-export _R_CHECK_LENGTH_1_LOGIC2_=package:_R_CHECK_PACKAGE_NAME_,abort,verbose
+#export _R_CHECK_LENGTH_1_LOGIC2_=package:_R_CHECK_PACKAGE_NAME_,abort,verbose
 export _R_CHECK_S3_METHODS_NOT_REGISTERED_=true
 #export _R_S3_METHOD_LOOKUP_BASEENV_AFTER_GLOBALENV_=true
+#export _R_CLASS_MATRIX_ARRAY_=true
 
