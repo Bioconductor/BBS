fb9950e971db3ed728f4dab29663d14aa065b700
Hervé Pagès <hpages@fredhutch.org>
2020-04-10 12:42:05 -0700

    Remove celaya2 build scripts from 3.11/

--- 3.11/bioc-longtests/machv2/config.sh
+++ 3.11/bioc-longtests/machv2/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# ====================
-# Settings for celaya2
-# ====================
+# ===================
+# Settings for machv2
+# ===================
 
 
 
@@ -9,13 +9,12 @@
 
 export BBS_DEBUG="0"
 
-export BBS_NODE_HOSTNAME="celaya2"
+export BBS_NODE_HOSTNAME="machv2"
 export BBS_USER="biocbuild"
 export BBS_RSAKEY="/Users/biocbuild/.BBS/id_rsa"
-export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.11-bioc"
+export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.11-bioc-longtests"
 export BBS_R_HOME="/Library/Frameworks/R.framework/Versions/Current/Resources"
-export BBS_NB_CPU=14        # 24 cores are available
-export BBS_CHECK_NB_CPU=18  # 24 cores are available
+export BBS_NB_CPU=4  # 24 cores are available
 
 
 
