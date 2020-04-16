6da3c7817e995cfc3c2964ce2da0b5a435f9dade
Hervé Pagès <hpages@fredhutch.org>
2020-04-16 14:04:14 -0700

    switch BioC 3.11 builds from master to RELEASE_3_11 branch of manifest

--- 3.11/data-experiment/config.bat
+++ 3.11/data-experiment/config.bat
@@ -20,7 +20,7 @@ set BBS_MEAT0_TYPE=3
 
 @rem Needed only if BBS_MEAT0_TYPE is 3
 set BBS_BIOC_MANIFEST_GIT_REPO_URL=https://git.bioconductor.org/admin/manifest
-set BBS_BIOC_MANIFEST_GIT_BRANCH=master
+set BBS_BIOC_MANIFEST_GIT_BRANCH=RELEASE_3_11
 set BBS_BIOC_MANIFEST_CLONE_PATH=%BBS_WORK_TOPDIR%\manifest
 
 @rem Needed if BBS_MEAT0_TYPE is 1 or 3
