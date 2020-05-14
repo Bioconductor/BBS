4c53d68396fb6630c7ba32b86716ff3e4c6c16ea
Hervé Pagès <hpages@fredhutch.org>
2020-05-14 09:55:14 -0700

    Add "new" merida1 (running Mojave) to the 3.12 software builds

--- 3.12/bioc/malbec1/config.sh
+++ 3.12/bioc/malbec1/config.sh
@@ -46,7 +46,7 @@ export BBS_GITLOG_RHOST="localhost"
 # packages to propagate and to later not be replaced by the bi-arch when
 # the dropped node is back.
 
-export BBS_OUTGOING_MAP="source:malbec1/buildsrc win.binary:tokay1/buildbin mac.binary:machv1/buildbin"
+export BBS_OUTGOING_MAP="source:malbec1/buildsrc win.binary:tokay1/buildbin mac.binary:merida1/buildbin"
 
 # Needed only on the node performing stage7a (BBS-make-STATUS_DB.py) and
 # stage8 (BBS-report.py)
@@ -54,7 +54,7 @@ export BBS_OUTGOING_MAP="source:malbec1/buildsrc win.binary:tokay1/buildbin mac.
 # IMPORTANT: BBS-report.py will treat BBS_REPORT_PATH as a _local_ path so it
 # must be run on the BBS_CENTRAL_RHOST machine.
 
-export BBS_REPORT_NODES="malbec1 tokay1:bin machv1:bin"
+export BBS_REPORT_NODES="malbec1 tokay1:bin merida1:bin"
 export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
 export BBS_REPORT_CSS="$BBS_HOME/$BBS_BIOC_VERSION/report.css"
 export BBS_REPORT_BGIMG="$BBS_HOME/images/DEVEL3b.png"
