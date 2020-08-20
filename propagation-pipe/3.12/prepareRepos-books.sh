48b539f425ebb9758551cee23887c0c24adad590
Hervé Pagès <hpages@fredhutch.org>
2020-08-20 01:13:55 -0700

    more tweaks

--- propagation-pipe/3.12/prepareRepos-books.sh
+++ propagation-pipe/3.12/prepareRepos-books.sh
@@ -5,7 +5,7 @@ cd "$HOME/propagation-pipe/3.12"
 . ./config.sh
 
 R_SCRIPT="library(tools)"
-R_SCRIPT="$R_SCRIPT; write_PACKAGES('$HOME/PACKAGES/$BIOC_VERSION/src/contrib')"
+R_SCRIPT="$R_SCRIPT; write_PACKAGES('$HOME/PACKAGES/$BIOC_VERSION/books/src/contrib')"
 
 echo "$R_SCRIPT" | $R --slave
 
