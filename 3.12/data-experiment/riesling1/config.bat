590ff02d92f0c95405ca333f22729e0fee31133a
Hervé Pagès <hpages@fredhutch.org>
2020-08-31 15:38:33 -0700

    Add riesling1 to the 3.12/data-experiment builds

---
+++ 3.12/data-experiment/riesling1/config.bat
@@ -0,0 +1,27 @@
+@rem ======================
+@rem Settings for riesling1
+@rem ======================
+
+
+set BBS_DEBUG=0
+
+set BBS_NODE_HOSTNAME=riesling1
+set BBS_USER=biocbuild
+set BBS_RSAKEY=D:\biocbuild\.BBS\id_rsa
+set BBS_WORK_TOPDIR=D:\biocbuild\bbs-3.12-data-experiment
+set BBS_R_HOME=D:\biocbuild\bbs-3.12-bioc\R
+set BBS_NB_CPU=12
+set BBS_CHECK_NB_CPU=16
+
+set BBS_STAGE2_MODE=multiarch
+set BBS_STAGE4_MODE=multiarch
+set BBS_STAGE5_MODE=multiarch
+
+
+
+@rem Shared settings (by all Windows nodes)
+
+set wd0=%cd%
+cd ..
+call config.bat
+cd %wd0%
