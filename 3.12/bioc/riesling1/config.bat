20ecda84bbf0993c5b8cc932835206312ed22e1f
Hervé Pagès <hpages@fredhutch.org>
2020-09-03 17:59:36 -0700

    Increase BBS_NB_CPU/BBS_CHECK_NB_CPU on riesling1 from 25/35 to 36/48 for the software builds

--- 3.12/bioc/riesling1/config.bat
+++ 3.12/bioc/riesling1/config.bat
@@ -10,8 +10,8 @@ set BBS_USER=biocbuild
 set BBS_RSAKEY=D:\biocbuild\.BBS\id_rsa
 set BBS_WORK_TOPDIR=D:\biocbuild\bbs-3.12-bioc
 set BBS_R_HOME=%BBS_WORK_TOPDIR%\R
-set BBS_NB_CPU=25
-set BBS_CHECK_NB_CPU=35
+set BBS_NB_CPU=36
+set BBS_CHECK_NB_CPU=48
 
 set BBS_STAGE2_MODE=multiarch
 set BBS_STAGE4_MODE=multiarch
