55143c7acfba3fb820308e8ecb68eda00c90cbec
Hervé Pagès <hpages.on.github@gmail.com>
2021-01-05 11:09:54 -0800

    Revert "small tweak to pushRepos-books.sh scripts"
    This reverts commit 76fe1ce82a5250c53b96002d1fcb401b1ab2f2df.

--- propagation-pipe/3.12/pushRepos-books.sh
+++ propagation-pipe/3.12/pushRepos-books.sh
@@ -8,5 +8,5 @@ REPOS_SUBDIR="books"
 REPOS_ROOT="$HOME/PACKAGES/$BIOC_VERSION/$REPOS_SUBDIR"
 DEST="webadmin@master.bioconductor.org:/extra/www/bioc/packages/$BIOC_VERSION/$REPOS_SUBDIR"
 
-rsync --delete -ave ssh $REPOS_ROOT/src $DEST
+rsync --delete -ave ssh $REPOS_ROOT/src $REPOS_ROOT/bin $DEST
 
