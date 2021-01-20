c11e7dec5c367f4d1c5332da8756ab7e3c38ac70
Hervé Pagès <hpages.on.github@gmail.com>
2021-01-20 14:12:04 -0800

    Add new BWH Linux machine rex3 to the BioC 3.13 builds

---
+++ nodes/rex3/local-settings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# =======================
+# Local settings for rex3
+# =======================
+
+
+if [ "$BBS_HOME" == "" ]; then
+    export BBS_HOME="/home/biocbuild/BBS"
+fi
+
+export BBS_PYTHON_CMD="/usr/bin/python3"
+
+export BBS_SSH_CMD="/usr/bin/ssh"
+export BBS_RSYNC_CMD="/usr/bin/rsync"
+
+# Needed only on a node capable of running STAGE1 (STAGE1 is supported on
+# Linux only)
+export BBS_SVN_CMD="/usr/bin/svn"
+export BBS_TAR_CMD="/bin/tar"
+export BBS_GIT_CMD="/usr/bin/git"
+
