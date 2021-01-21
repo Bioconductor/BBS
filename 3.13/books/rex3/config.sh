0be2150542f83f108b405e5abed2182801982ff7
Hervé Pagès <hpages.on.github@gmail.com>
2021-01-21 09:06:00 -0800

    adjust BBS_NB_CPU for rex3

--- 3.13/books/rex3/config.sh
+++ 3.13/books/rex3/config.sh
@@ -14,7 +14,7 @@ export BBS_USER="biocbuild"
 export BBS_RSAKEY="/home/biocbuild/.BBS/id_rsa"
 export BBS_WORK_TOPDIR="/home/biocbuild/bbs-3.13-books"
 export BBS_R_HOME="/home/biocbuild/bbs-3.13-bioc/R"
-export BBS_NB_CPU=3  # 20 cores are available
+export BBS_NB_CPU=15  # 80 cores are available
 
 
 
