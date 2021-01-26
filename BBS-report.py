9f0e4a99d7b48f3aa2dbb042bde6241305eb01e8
Hervé Pagès <hpages.on.github@gmail.com>
2021-01-25 21:30:22 -0800

    cosmetic

--- BBS-report.py
+++ BBS-report.py
@@ -572,7 +572,7 @@ def write_goback_asHTML(out, href, current_letter=None):
     report_nodes = BBSutils.getenv('BBS_REPORT_NODES')
     title = BBSreportutils.make_report_title(report_nodes)
     out.write('<TABLE class="grid_layout"')
-    out.write(' style="width: 100%; background: #BBB;"><TR>')
+    out.write(' style="width: 100%; background: #EEE;"><TR>')
     out.write('<TD style="text-align: left; padding-left: 5px; vertical-align: middle;">')
     out.write('<I><A href="%s">Back to <B>%s</B></A></I>' % (href, title))
     out.write('</TD>')
