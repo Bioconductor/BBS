ebc15bee0c05c13a9a80d679310c2978678b998f
Hervé Pagès <hpages.on.github@gmail.com>
2021-01-27 15:03:18 -0500

    better error handling

--- propagation-pipe/BBS-deploy-books.py
+++ propagation-pipe/BBS-deploy-books.py
@@ -11,10 +11,19 @@ import bbs.parse
 import bbs.jobs
 
 def _deploy_book(pkg, version, dest_dir):
-    dest_subdir = os.path.join(dest_dir, pkg)
-    print('Deploying %s book at \'%s/\' ...' % (pkg, dest_subdir), end=' ')
-    sys.stdout.flush()
     srcpkg_file = '%s_%s.tar.gz' % (pkg, version)
+    dest_subdir = os.path.join(dest_dir, pkg)
+    print('Deploying content from book tarball \'%s\' to \'%s/\' ...' % \
+          (srcpkg_file, dest_subdir), end=' ')
+    sys.stdout.flush()
+    if not os.path.exists(srcpkg_file):
+        errmsg = 'oops.. couldn\'t find book tarball \'%s\' ' % srcpkg_file + \
+                 'in\ndirectory:\n\n  %s\n\n' % os.getcwd() + \
+                 'Most likely this means that the package index ' + \
+                 '(PACKAGES file) is out-of-sync\nwith the content of ' + \
+                 'the directory.\n\nDid you run prepareRepos-books.sh ' + \
+                 'before trying to deploy the books?'
+        raise Exception(errmsg)
     tar = tarfile.open(srcpkg_file)
     tar.extractall()
 
