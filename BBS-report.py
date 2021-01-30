d2e30fac9056f8508dbf9c500e8318a84a45e9d8
Hervé Pagès <hpages.on.github@gmail.com>
2021-01-29 23:15:19 -0800

    small tweak

--- BBS-report.py
+++ BBS-report.py
@@ -369,7 +369,7 @@ def write_gcard(out, pkg, pkg_pos, nb_pkgs, leafreport_ref,
                 version = maintainer = status = ''
             deprecated = status == "Deprecated"
             out.write('<TD ROWSPAN="%d" style="vertical-align: top;">' % \
-                      nb_nodes + 1)
+                      (nb_nodes + 1))
             out.write(pkgname_and_version_to_HTML(pkg, version, deprecated))
             out.write('<BR>%s' % maintainer)
             if (BBSvars.MEAT0_type == 1 or BBSvars.MEAT0_type == 3):
@@ -391,7 +391,7 @@ def write_gcard(out, pkg, pkg_pos, nb_pkgs, leafreport_ref,
     out.write('<TR>')
     out.write('<TD class="bottom_left_corner"></TD>')
     colspan = BBSreportutils.ncol_to_display(BBSvars.subbuilds) + 2
-    out.write('<TD COLSPAN="%s"></TD>' % colspan)
+    out.write('<TD COLSPAN="%d"></TD>' % colspan)
     out.write('<TD class="bottom_right_corner"></TD>')
     out.write('</TR>\n')
     out.write('</TBODY>\n')
