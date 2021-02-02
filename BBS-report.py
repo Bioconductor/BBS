d51754824b964936c4607543ea75bac2ea6c3c03
Hervé Pagès <hpages.on.github@gmail.com>
2021-02-01 21:41:02 -0800

    small tweak

--- BBS-report.py
+++ BBS-report.py
@@ -142,7 +142,8 @@ def _write_glyph_box(out, status, toggleable=False):
                     #'onkeypress="%s"' % onclick,
                     'onclick="%s"' % onclick]
         checkbox_id = '%s_checkbox' % status.lower()
-        checkbox_attrs = 'id="%s" style="padding: 0px;"' % checkbox_id
+        checkbox_attrs = 'id="%s" style="margin: 0px; padding: 0px;"' % \
+                         checkbox_id
         checkbox_html = '<INPUT type="checkbox" %s>' % checkbox_attrs
     else:
         TD_attrs = ['class="glyph_box"']
