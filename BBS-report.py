3e216411c7343f3e0ce8b569deb9c624d03667d9
Hervé Pagès <hpages.on.github@gmail.com>
2021-02-01 21:46:00 -0800

    small tweak

--- BBS-report.py
+++ BBS-report.py
@@ -148,7 +148,7 @@ def _write_glyph_box(out, status, toggleable=False):
     else:
         TD_attrs = ['class="glyph_box"']
         checkbox_html = ''
-    TRcontent  = '<TD style="width: 100px; padding-left: 3px;">%s</TD>' % \
+    TRcontent  = '<TD style="width: 95px; padding-left: 3px; padding-right: 3px;">%s</TD>' % \
                  _status_as_glyph(status)
     TRcontent += '<TD style="width: 16px;">%s</TD>' % checkbox_html
     TDcontent = '<TABLE><TR>%s</TR></TABLE>' % TRcontent
