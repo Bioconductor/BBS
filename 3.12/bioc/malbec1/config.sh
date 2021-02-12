f10fd55be5f06e8ac3c752349fc69f630d3fa913
Hervé Pagès <hpages.on.github@gmail.com>
2021-02-12 00:41:38 -0800

    Remove riesling1 from the 3.12/bioc builds

--- 3.12/bioc/malbec1/config.sh
+++ 3.12/bioc/malbec1/config.sh
@@ -54,7 +54,7 @@ export BBS_OUTGOING_MAP="source:malbec1/buildsrc win.binary:tokay1/buildbin mac.
 # IMPORTANT: BBS-report.py will treat BBS_REPORT_PATH as a _local_ path so it
 # must be run on the BBS_CENTRAL_RHOST machine.
 
-export BBS_REPORT_NODES="malbec1 nebbiolo1 tokay1:bin riesling1:bin merida1:bin"
+export BBS_REPORT_NODES="malbec1 nebbiolo1 tokay1:bin merida1:bin"
 export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
 export BBS_REPORT_CSS="$BBS_HOME/$BBS_BIOC_VERSION/report.css"
 export BBS_REPORT_BGIMG="$BBS_HOME/images/DEVEL3b.png"
