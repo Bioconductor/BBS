2e1fa176a3bd9cb5122037bcbc5d8cf4fa0a179b
Hervé Pagès <hpages.on.github@gmail.com>
2021-03-08 16:55:08 -0800

    turn malbec2 into a standalone alternate builder for the 3.13/bioc builds

--- 3.13/bioc/malbec2/config.sh
+++ 3.13/bioc/malbec2/config.sh
@@ -32,28 +32,13 @@ cd "$wd0"
 
 
 
-# Needed only on the node performing stage6 (must be run on the
-# BBS_CENTRAL_RHOST machine).
-#
-
-# If you are NOT using a bi-arch builder (i.e. moscato1 or moscato2) to
-# create your Windows build products, the following note applies:
-
-# IMPORTANT NOTE: The win.binary mapping must either include the 2
-# Windows nodes (win32 and win64) or not be present at all. Temporarily
-# dropping a Windows node will allow some single-arch Windows binary
-# packages to propagate and to later not be replaced by the bi-arch when
-# the dropped node is back.
-
-export BBS_OUTGOING_MAP="source:malbec2/buildsrc win.binary:tokay2/buildbin mac.binary:machv2/buildbin"
-
 # Needed only on the node performing stage7a (BBS-make-STATUS_DB.py) and
 # stage8 (BBS-report.py)
 #
 # IMPORTANT: BBS-report.py will treat BBS_REPORT_PATH as a _local_ path so it
 # must be run on the BBS_CENTRAL_RHOST machine.
 
-export BBS_REPORT_NODES="malbec2 tokay2:bin machv2:bin"
+export BBS_REPORT_NODES="malbec2"
 export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
 export BBS_REPORT_CSS="$BBS_HOME/$BBS_BIOC_VERSION/report.css"
 export BBS_REPORT_BGIMG="$BBS_HOME/images/DEVEL3b.png"
@@ -61,13 +46,13 @@ export BBS_REPORT_JS="$BBS_HOME/$BBS_BIOC_VERSION/report.js"
 #export BBS_REPORT_MOTD="Happy new year to all Bioconductor developers!"
 
 # Where to publish the report
-export BBS_PUBLISHED_REPORT_RELATIVEURL="checkResults/$BBS_BIOC_VERSION/$BBS_SUBBUILDS-LATEST/"
+export BBS_PUBLISHED_REPORT_RELATIVEURL="checkResults/$BBS_BIOC_VERSION/malbec2/$BBS_SUBBUILDS-LATEST/"
 export BBS_PUBLISHED_REPORT_DEST_DIR="webadmin@master.bioconductor.org:/extra/www/bioc/$BBS_PUBLISHED_REPORT_RELATIVEURL"
 
 
 # Needed only on the node performing stage9 (BBS-notify.py)
 
 # TODO: when BBS_NOTIFY_NODES is not defined then take all the build nodes
-export BBS_NOTIFY_NODES="malbec2"
-export BBS_PUBLISHED_REPORT_URL="https://master.bioconductor.org/$BBS_PUBLISHED_REPORT_RELATIVEURL"
+#export BBS_NOTIFY_NODES="malbec2"
+#export BBS_PUBLISHED_REPORT_URL="https://master.bioconductor.org/$BBS_PUBLISHED_REPORT_RELATIVEURL"
 
