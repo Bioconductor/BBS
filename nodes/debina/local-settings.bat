d5f3eb1a3774fa3b433eeece40df9d1526dcc335
Hervé Pagès <hpages.on.github@gmail.com>
2021-04-07 19:58:04 -0700

    Add debina (Windows Azure VM) to the 3.13/bioc builds

---
+++ nodes/debina/local-settings.bat
@@ -0,0 +1,13 @@
+@rem =========================
+@rem Local settings for debina
+@rem =========================
+
+
+set BBS_HOME=D:\biocbuild\BBS
+
+set BBS_PYTHON_CMD=C:\Python38\python.exe
+
+set BBS_SSH_CMD=C:\cygwin\bin\ssh.exe
+set BBS_RSYNC_CMD=C:\cygwin\bin\rsync.exe 
+set BBS_CURL_CMD=C:\cygwin\bin\curl.exe
+
