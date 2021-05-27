cb8a4672e44f417d3e4ab80fb0eaa8609101df5e
Hervé Pagès <hpages.on.github@gmail.com>
2021-05-27 13:54:18 -0700

    add merida1 to the 3.14/workflows builds

--- 3.14/bioc/merida1/config.sh
+++ 3.14/bioc/merida1/config.sh
@@ -12,7 +12,7 @@ export BBS_DEBUG="0"
 export BBS_NODE_HOSTNAME="merida1"
 export BBS_USER="biocbuild"
 export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.14-bioc"
-export BBS_R_HOME="/Library/Frameworks/R.framework/Versions/Current/Resources"
+export BBS_R_HOME="/Library/Frameworks/R.framework/Resources"
 export BBS_NB_CPU=14        # 24 cores are available
 export BBS_CHECK_NB_CPU=16  # 24 cores are available
 
