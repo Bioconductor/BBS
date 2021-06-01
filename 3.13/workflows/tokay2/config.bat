f3c06490d51aabe66dbc5b00a54533e744e02cfd
Hervé Pagès <hpages.on.github@gmail.com>
2021-06-01 16:44:28 -0700

    use tokay2 instead of riesling1 for the 3.13 workflows builds

--- 3.13/workflows/tokay2/config.bat
+++ 3.13/workflows/tokay2/config.bat
@@ -1,21 +1,21 @@
-@rem ======================
-@rem Settings for riesling1
-@rem ======================
+@rem ===================
+@rem Settings for tokay2
+@rem ===================
 
 
 set BBS_DEBUG=0
 
-set BBS_NODE_HOSTNAME=riesling1
+set BBS_NODE_HOSTNAME=tokay2
 set BBS_USER=biocbuild
-set BBS_WORK_TOPDIR=D:\biocbuild\bbs-3.13-workflows
-set BBS_R_HOME=D:\biocbuild\bbs-3.13-bioc\R
-set BBS_NB_CPU=12
+set BBS_WORK_TOPDIR=C:\Users\biocbuild\bbs-3.13-workflows
+set BBS_R_HOME=C:\Users\biocbuild\bbs-3.13-bioc\R
+set BBS_NB_CPU=10
 
 set BBS_STAGE2_MODE=multiarch
 
 @rem Central build node is nebbiolo1 at DFCI.
 set BBS_CENTRAL_RHOST=nebbiolo1
-set BBS_RSH_CMD=ssh -F /cygdrive/d/biocbuild/.ssh/config
+set BBS_RSH_CMD=ssh -F /cygdrive/c/Users/biocbuild/.ssh/config
 set BBS_CENTRAL_ROOT_URL=http://155.52.207.165
 
 @rem Source tarballs produced during STAGE3 won't be propagated
