00e325b5e73ac7f21684db3c8a25d352a1817808
J Wokaty <jwokaty@users.noreply.github.com>
2021-10-20 13:03:03 -0400

    Add config files and scripts for 3.15 builds

---
+++ 3.15/bioc-longtests/riesling1/config.bat
@@ -0,0 +1,28 @@
+@rem ======================
+@rem Settings for riesling1
+@rem ======================
+
+
+set BBS_DEBUG=0
+
+set BBS_NODE_HOSTNAME=riesling1
+set BBS_USER=biocbuild
+set BBS_WORK_TOPDIR=D:\biocbuild\bbs-3.15-bioc-longtests
+set BBS_R_HOME=D:\biocbuild\bbs-3.15-bioc\R
+set BBS_NB_CPU=10
+
+set BBS_STAGE4_MODE=multiarch
+
+@rem Central build node is nebbiolo1 at DFCI.
+set BBS_CENTRAL_RHOST=nebbiolo1
+set BBS_RSH_CMD=ssh -F /cygdrive/d/biocbuild/.ssh/config
+set BBS_CENTRAL_ROOT_URL=http://155.52.207.166
+
+
+
+@rem Shared settings (by all Windows nodes)
+
+set wd0=%cd%
+cd ..
+call config.bat
+cd %wd0%
---
+++ propagation/3.15/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# =========================================================
+# Settings shared by all scripts in propagation/3.15 folder
+# =========================================================
+
+BIOC_VERSION="3.15"
+R_VERSION="4.2"
+Rscript="$HOME/bin/Rscript-$R_VERSION"
