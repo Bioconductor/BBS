216f011ce3b5c4cc2dff79f725638cc472585c05
Hervé Pagès <hpages.on.github@gmail.com>
2021-11-10 09:06:44 -0800

    Set BBS_NB_CPU/BBS_CHECK_NB_CPU back to 15/18 on machv2 for the 3.14 software builds

--- 3.14/bioc/machv2/config.sh
+++ 3.14/bioc/machv2/config.sh
@@ -13,8 +13,8 @@ export BBS_NODE_HOSTNAME="machv2"
 export BBS_USER="biocbuild"
 export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.14-bioc"
 export BBS_R_HOME="/Library/Frameworks/R.framework/Resources"
-export BBS_NB_CPU=16        # 24 cores are available
-export BBS_CHECK_NB_CPU=20  # 24 cores are available
+export BBS_NB_CPU=15        # 24 cores are available
+export BBS_CHECK_NB_CPU=18  # 24 cores are available
 
 # Central build node is nebbiolo2 at DFCI.
 export BBS_CENTRAL_RHOST="nebbiolo2"
