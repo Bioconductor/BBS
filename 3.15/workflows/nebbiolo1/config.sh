88dbf96b9b9bbb74d0f067d58991df2c94a13971
J Wokaty <jwokaty@users.noreply.github.com>
2021-11-10 11:32:21 -0500

    Workflows linux builders in OUTGOING_MAP

--- 3.15/workflows/nebbiolo1/config.sh
+++ 3.15/workflows/nebbiolo1/config.sh
@@ -35,11 +35,11 @@ cd "$wd0"
 # central node
 
 # Control propagation:
-export BBS_OUTGOING_MAP="source:nebbiolo1/buildsrc mac.binary:merida1/buildbin"
+export BBS_OUTGOING_MAP="source:nebbiolo1/buildsrc"
 export BBS_FINAL_REPO="file://home/biocpush/PACKAGES/$BBS_BIOC_VERSION/workflows"
 
 # Control generation of the report:
-export BBS_REPORT_NODES="nebbiolo1 merida1:bin"
+export BBS_REPORT_NODES="nebbiolo1 merida1"
 export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
 export BBS_REPORT_CSS="$BBS_HOME/$BBS_BIOC_VERSION/report.css"
 export BBS_REPORT_BGIMG="$BBS_HOME/images/DEVEL3b.png"
