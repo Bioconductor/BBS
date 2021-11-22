1af628f45cfd5ac3513e22ce1d79dbba0bc6ce84
Hervé Pagès <hpages.on.github@gmail.com>
2021-11-22 13:09:57 -0800

    Remove riesling1 from the 3.14 software builds

--- 3.14/bioc/nebbiolo2/config.sh
+++ 3.14/bioc/nebbiolo2/config.sh
@@ -39,7 +39,7 @@ export BBS_OUTGOING_MAP="source:nebbiolo2/buildsrc win.binary:tokay2/buildbin ma
 export BBS_FINAL_REPO="file://home/biocpush/PACKAGES/$BBS_BIOC_VERSION/bioc"
 
 # Control generation of the report:
-export BBS_REPORT_NODES="nebbiolo2 tokay2:bin riesling1:bin machv2:bin"
+export BBS_REPORT_NODES="nebbiolo2 tokay2:bin machv2:bin"
 export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
 export BBS_REPORT_CSS="$BBS_HOME/$BBS_BIOC_VERSION/report.css"
 export BBS_REPORT_BGIMG="$BBS_HOME/images/DEVEL3b.png"
