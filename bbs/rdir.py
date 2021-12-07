Bioconductor/BBS

commit 7fb48817400d91f87516a230fc16fc2ba68023eb

Improve handling of rsync options

bbs/rdir.py
@@ -50,7 +50,9 @@ class RemoteDir:
     # remote dir (located at the URL specified by 'url'). In that case all
     # the remaining args are ignored and the remote dir can only be accessed
     # in read-only mode (via the WOpen method).
-    def __init__(self, label, url, path, host, user, rsh_cmd, rsync_cmd, rsync_rsh_cmd):
+    def __init__(self, label, url,
+                 path=None, host=None, user=None, rsh_cmd=None,
+                 rsync_cmd=None, rsync_rsh_cmd=None, rsync_options=None):
         self.label = label
         self.url = url
         self.path = path
@@ -59,6 +61,8 @@ class RemoteDir:
         self.rsh_cmd = rsh_cmd
         self.rsync_cmd = rsync_cmd
         self.rsync_rsh_cmd = rsync_rsh_cmd
+        self.rsync_options = rsync_options
+        return
 
     def subdir(self, subdir):
         url = self.url
@@ -67,10 +71,9 @@ class RemoteDir:
         path = self.path
         if path != None:
             path += '/' + subdir
-        return RemoteDir(self.label + '/' + subdir,
-                         url, path,
-                         self.host, self.user,
-                         self.rsh_cmd, self.rsync_cmd, self.rsync_rsh_cmd)
+        return RemoteDir(self.label + '/' + subdir, url,
+                         path, self.host, self.user, self.rsh_cmd,
+                         self.rsync_cmd, self.rsync_rsh_cmd, self.rsync_options)
 
     # Open local or remote file in binary mode.
     def WOpen(self, file, return_None_on_error=False):
@@ -125,11 +128,13 @@ class RemoteDir:
         if self.host == None or self.host == 'localhost':
             # self is a local dir
             src_path = os.path.join(self.path, src_path)
-            cmd = "%s %s %s" % (self.rsync_cmd, src_path, dest_path)
+            cmd = "%s %s %s %s" % \
+                (self.rsync_cmd, self.rsync_options, src_path, dest_path)
         else:
             # self is a remote dir
             src_path = "%s/%s" % (self.get_full_remote_path(), src_path)
-            cmd = "%s %s %s" % (self.rsync_rsh_cmd, src_path, dest_path)
+            cmd = "%s %s %s %s" % \
+                (self.rsync_rsh_cmd, self.rsync_options, src_path, dest_path)
         jobs.tryHardToRunJob(cmd, 5, None, 60.0, 20.0, True, verbose)
         return
 
@@ -210,10 +215,13 @@ class RemoteDir:
         set_readable_flag(src_path)
         if self.host == None or self.host == 'localhost':
             # self is a local dir
-            cmd = "%s %s %s" % (self.rsync_cmd, src_path, self.path)
+            cmd = "%s %s %s %s" % \
+                (self.rsync_cmd, self.rsync_options, src_path, self.path)
         else:
             # self is a remote dir
-            cmd = "%s %s %s" % (self.rsync_rsh_cmd, src_path, self.get_full_remote_path())
+            cmd = "%s %s %s %s" % \
+                (self.rsync_rsh_cmd, self.rsync_options, src_path,
+                 self.get_full_remote_path())
         maxtime = 120.0 + fileutils.total_size(src_path) / bandwidth_in_bytes_per_sec
         if verbose:
             if self.host == None or self.host == 'localhost':
@@ -245,12 +253,13 @@ class RemoteDir:
             # self is a remote dir
             rsync_cmd = self.rsync_rsh_cmd
             src = self.get_full_remote_path()
+        rsync_options = self.rsync_options
         if sys.platform == "win32":
             # Transform symlink into referent file/dir (-L)
-            rsync_options = '-rLptz'
+            rsync_options += '-rLptz'
         else:
             # Copy symlinks as symlinks (-l)
-            rsync_options = '-rlptz'
+            rsync_options += '-rlptz'
         cmd = "%s %s %s/ %s" % (rsync_cmd, rsync_options, src, '.')
         if verbose:
             print("BBS>   Syncing local '%s' with %s" % (local_dir, self.label))

3.14/config.bat
@@ -8,8 +8,8 @@
 
 call ..\nodes\%BBS_NODE_HOSTNAME%\local-settings.bat
 
-set BBS_RSYNC_CMD=%BBS_RSYNC_CMD% -r --delete --exclude='.svn' --exclude='.git' --exclude='.github' --exclude='.git_*'
 set BBS_RSYNC_RSH_CMD=%BBS_RSYNC_CMD% --rsh '%BBS_RSH_CMD%'
+set BBS_RSYNC_OPTIONS=-r --delete --exclude='.svn' --exclude='.git' --exclude='.github' --exclude='.git_*'
 
 set BBS_R_CMD=%BBS_R_HOME%\bin\R.exe
 set BBS_RSCRIPT_CMD=%BBS_R_HOME%\bin\Rscript.exe

3.14/config.sh
@@ -9,8 +9,8 @@
 
 . "../nodes/$BBS_NODE_HOSTNAME/local-settings.sh"
 
-export BBS_RSYNC_CMD="$BBS_RSYNC_CMD -rl --delete --exclude='.svn' --exclude='.git' --exclude='.github' --exclude='.git_*'"
 export BBS_RSYNC_RSH_CMD="$BBS_RSYNC_CMD --rsh '$BBS_RSH_CMD'"
+export BBS_RSYNC_OPTIONS="-rl --delete --exclude='.svn' --exclude='.git' --exclude='.github' --exclude='.git_*'"
 
 export BBS_R_CMD="$BBS_R_HOME/bin/R"
 export BBS_RSCRIPT_CMD="$BBS_R_HOME/bin/Rscript"

3.15/config.bat
@@ -8,8 +8,8 @@
 
 call ..\nodes\%BBS_NODE_HOSTNAME%\local-settings.bat
 
-set BBS_RSYNC_CMD=%BBS_RSYNC_CMD% -r --delete --exclude='.svn' --exclude='.git' --exclude='.github' --exclude='.git_*'
 set BBS_RSYNC_RSH_CMD=%BBS_RSYNC_CMD% --rsh '%BBS_RSH_CMD%'
+set BBS_RSYNC_OPTIONS=-r --delete --exclude='.svn' --exclude='.git' --exclude='.github' --exclude='.git_*'
 
 set BBS_R_CMD=%BBS_R_HOME%\bin\R.exe
 set BBS_RSCRIPT_CMD=%BBS_R_HOME%\bin\Rscript.exe

3.15/config.sh
@@ -9,8 +9,8 @@
 
 . "../nodes/$BBS_NODE_HOSTNAME/local-settings.sh"
 
-export BBS_RSYNC_CMD="$BBS_RSYNC_CMD -rl --delete --exclude='.svn' --exclude='.git' --exclude='.github' --exclude='.git_*'"
 export BBS_RSYNC_RSH_CMD="$BBS_RSYNC_CMD --rsh '$BBS_RSH_CMD'"
+export BBS_RSYNC_OPTIONS="-rl --delete --exclude='.svn' --exclude='.git' --exclude='.github' --exclude='.git_*'"
 
 export BBS_R_CMD="$BBS_R_HOME/bin/R"
 export BBS_RSCRIPT_CMD="$BBS_R_HOME/bin/Rscript"

BBS-run.py
@@ -351,7 +351,8 @@ def STAGE2_loop(job_queue, nb_cpu, out_dir):
     if asynchronous_mode:
         rdir = BBSvars.install_rdir
         dest = rdir.get_full_remote_path()
-        products_push_cmd = '%s -v %s/ %s' % (rdir.rsync_rsh_cmd, out_dir, dest)
+        products_push_cmd = '%s -av %s/ %s' % \
+                            (rdir.rsync_rsh_cmd, out_dir, dest)
         products_push_log = os.path.join(products_out_buf, 'install-push.log')
     else:
         products_push_cmd = products_push_log = None
@@ -507,7 +508,8 @@ def STAGE3_loop(job_queue, nb_cpu, out_dir):
     if asynchronous_mode:
         rdir = BBSvars.buildsrc_rdir
         dest = rdir.get_full_remote_path()
-        products_push_cmd = "%s -v %s/ %s" % (rdir.rsync_rsh_cmd, out_dir, dest)
+        products_push_cmd = "%s -av %s/ %s" % \
+                            (rdir.rsync_rsh_cmd, out_dir, dest)
         products_push_log = os.path.join(products_out_buf, 'buildsrc-push.log')
     else:
         products_push_cmd = products_push_log = None
@@ -591,7 +593,8 @@ def STAGE4_loop(job_queue, nb_cpu, out_dir):
     if asynchronous_mode:
         rdir = BBSvars.checksrc_rdir
         dest = rdir.get_full_remote_path()
-        products_push_cmd = "%s -v %s/ %s" % (rdir.rsync_rsh_cmd, out_dir, dest)
+        products_push_cmd = "%s -av %s/ %s" % \
+                            (rdir.rsync_rsh_cmd, out_dir, dest)
         products_push_log = os.path.join(products_out_buf, 'checksrc-push.log')
     else:
         products_push_cmd = products_push_log = None
@@ -661,7 +664,8 @@ def STAGE5_loop(job_queue, nb_cpu, out_dir):
     if asynchronous_mode:
         rdir = BBSvars.buildbin_rdir
         dest = rdir.get_full_remote_path()
-        products_push_cmd = "%s -v %s/ %s" % (rdir.rsync_rsh_cmd, out_dir, dest)
+        products_push_cmd = "%s -av %s/ %s" % \
+                            (rdir.rsync_rsh_cmd, out_dir, dest)
         products_push_log = os.path.join(products_out_buf, 'buildbin-push.log')
     else:
         products_push_cmd = products_push_log = None

BBSvars.py
@@ -73,24 +73,19 @@ BUILDBIN_timeout = float(BBSutils.getenv('BBS_BUILDBIN_TIMEOUT', False,
 rsh_cmd = BBSutils.getenv('BBS_RSH_CMD', False)
 rsync_cmd = BBSutils.getenv('BBS_RSYNC_CMD')
 rsync_rsh_cmd = BBSutils.getenv('BBS_RSYNC_RSH_CMD')
+rsync_options = BBSutils.getenv('BBS_RSYNC_OPTIONS')
 
 central_rdir_path = BBSutils.getenv('BBS_CENTRAL_RDIR', False)
 if central_rdir_path == None:
     Central_rdir = bbs.rdir.RemoteDir('BBS_CENTRAL_BASEURL',
-                    BBSutils.getenv('BBS_CENTRAL_BASEURL'),
-                    None,
-                    None,
-                    None,
-                    None, None, None)
+                                      BBSutils.getenv('BBS_CENTRAL_BASEURL'))
 else:
     central_rhost = BBSutils.getenv('BBS_CENTRAL_RHOST', False)
     central_ruser = BBSutils.getenv('BBS_CENTRAL_RUSER', False)
     Central_rdir = bbs.rdir.RemoteDir('BBS_CENTRAL_BASEURL',
                     BBSutils.getenv('BBS_CENTRAL_BASEURL'),
-                    central_rdir_path,
-                    central_rhost,
-                    central_ruser,
-                    rsh_cmd, rsync_cmd, rsync_rsh_cmd)
+                    central_rdir_path, central_rhost, central_ruser, rsh_cmd,
+                    rsync_cmd, rsync_rsh_cmd, rsync_options)
 
 products_in_rdir = Central_rdir.subdir('products-in')
 
@@ -102,8 +97,7 @@ MEAT0_rdir = bbs.rdir.RemoteDir('BBS_MEAT0_RDIR',
                 BBSutils.getenv('BBS_MEAT0_RHOST', False),
                 BBSutils.getenv('BBS_MEAT0_RUSER', False),
                 rsh_cmd,
-                rsync_cmd,
-                rsync_rsh_cmd)
+                rsync_cmd, rsync_rsh_cmd, rsync_options)
 
 meat_path = BBSutils.getenv('BBS_MEAT_PATH')
 
@@ -169,6 +163,5 @@ GITLOG_rdir = bbs.rdir.RemoteDir('BBS_GITLOG_RDIR',
                 BBSutils.getenv('BBS_GITLOG_RHOST', False),
                 BBSutils.getenv('BBS_GITLOG_RUSER', False),
                 rsh_cmd,
-                rsync_cmd,
-                rsync_rsh_cmd)
+                rsync_cmd, rsync_rsh_cmd, rsync_options)
 

commit 73cad7972c970bc22cf5b288d8bc5899140ef40c

Fix regression in handling of rsync options (introduced in commit 7fb4881)

bbs/rdir.py
@@ -256,10 +256,10 @@ class RemoteDir:
         rsync_options = self.rsync_options
         if sys.platform == "win32":
             # Transform symlink into referent file/dir (-L)
-            rsync_options += '-rLptz'
+            rsync_options += ' -rLptz'
         else:
             # Copy symlinks as symlinks (-l)
-            rsync_options += '-rlptz'
+            rsync_options += ' -rlptz'
         cmd = "%s %s %s/ %s" % (rsync_cmd, rsync_options, src, '.')
         if verbose:
             print("BBS>   Syncing local '%s' with %s" % (local_dir, self.label))