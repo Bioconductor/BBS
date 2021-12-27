dd38f16a7adc58ec25c2183403748513291883ae
J Wokaty <jwokaty@users.noreply.github.com>
2021-12-27 12:29:21 -0500

    Add palomino3 local-settings.bat

---
+++ nodes/palomino3/local-settings.bat
@@ -0,0 +1,13 @@
+@rem ============================
+@rem Local settings for palomino3
+@rem ============================
+
+
+set BBS_HOME=F:\biocbuild\BBS
+
+set BBS_PYTHON_CMD=C:\Python39\python.exe
+
+set BBS_SSH_CMD=C:\cygwin\bin\ssh.exe
+set BBS_RSYNC_CMD=C:\cygwin\bin\rsync.exe 
+set BBS_CURL_CMD=C:\cygwin\bin\curl.exe
+
