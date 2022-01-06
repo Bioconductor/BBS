059f8ac4a15e0ee6694bdd557af24fbf3aa4d882
Hervé Pagès <hpages.on.github@gmail.com>
2022-01-05 16:06:52 -0800

    Slightly reduce the *_NB_CPU values on the nebbiolos for the software and data-experiment builds

--- 3.14/bioc/nebbiolo2/config.sh
+++ 3.14/bioc/nebbiolo2/config.sh
@@ -13,8 +13,8 @@ export BBS_NODE_HOSTNAME="nebbiolo2"
 export BBS_USER="biocbuild"
 export BBS_WORK_TOPDIR="/home/biocbuild/bbs-3.14-bioc"
 export BBS_R_HOME="$BBS_WORK_TOPDIR/R"
-export BBS_NB_CPU=25        # 72 cores are available
-export BBS_CHECK_NB_CPU=32  # 72 cores are available
+export BBS_NB_CPU=24        # 72 cores are available
+export BBS_CHECK_NB_CPU=30  # 72 cores are available
 
 export BBS_CENTRAL_RHOST="localhost"
 export BBS_CENTRAL_ROOT_URL="http://$BBS_CENTRAL_RHOST"
