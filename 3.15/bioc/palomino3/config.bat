3797cafb27150fc1bdf9b9c99a42796feb591364
J Wokaty <jwokaty@users.noreply.github.com>
2022-01-10 11:53:35 -0500

    Increase Palomino3's CPUs

--- 3.15/bioc/palomino3/config.bat
+++ 3.15/bioc/palomino3/config.bat
@@ -9,9 +9,9 @@ set BBS_NODE_HOSTNAME=palomino3
 set BBS_USER=biocbuild
 set BBS_WORK_TOPDIR=F:\biocbuild\bbs-3.15-bioc
 set BBS_R_HOME=%BBS_WORK_TOPDIR%\R
-set BBS_NB_CPU=14
-set BBS_BUILD_NB_CPU=12
-set BBS_CHECK_NB_CPU=13
+set BBS_NB_CPU=21
+set BBS_BUILD_NB_CPU=18
+set BBS_CHECK_NB_CPU=20
 
 @rem set BBS_STAGE2_MODE=multiarch
 @rem set BBS_STAGE4_MODE=multiarch
