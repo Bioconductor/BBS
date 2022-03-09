2e9559e2e1aec040bc929a22b83127d0ee39691b
Hervé Pagès <hpages.on.github@gmail.com>
2022-03-08 16:38:03 -0800

    clean up

--- 3.15/bioc/palomino/testR.bat
+++
@@ -1,4 +0,0 @@
-call config.bat
-
-%BBS_R_HOME%\bin\Rscript --vanilla -e "cat('TMPDIR=',Sys.getenv('TMPDIR'),'\n',sep='');cat('TMP=',Sys.getenv('TMP'),'\n',sep='');cat('TEMP=',Sys.getenv('TEMP'),'\n',sep='');cat('tempdir(): ',tempdir(),'\n',sep='')"
-
