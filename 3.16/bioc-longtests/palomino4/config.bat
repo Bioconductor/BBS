597f12537d02190d0ab5e9b0beca99b4f5411e4d
J Wokaty <jwokaty@users.noreply.github.com>
2022-04-26 19:52:46 -0400

    Fix ssh path for palomino4 3.16

--- 3.16/bioc-longtests/palomino4/config.bat
+++ 3.16/bioc-longtests/palomino4/config.bat
@@ -7,15 +7,15 @@ set BBS_DEBUG=0
 
 set BBS_NODE_HOSTNAME=palomino4
 set BBS_USER=biocbuild
-set BBS_WORK_TOPDIR=C:\Users\biocbuild\bbs-3.16-bioc-longtests
-set BBS_R_HOME=C:\Users\biocbuild\bbs-3.16-bioc\R
+set BBS_WORK_TOPDIR=F:\biocbuild\bbs-3.16-bioc-longtests
+set BBS_R_HOME=F:\biocbuild\bbs-3.16-bioc\R
 set BBS_NB_CPU=8
 
 set BBS_STAGE4_MODE=multiarch
 
 @rem Central build node is nebbiolo2 at DFCI.
 set BBS_CENTRAL_RHOST=nebbiolo2
-set BBS_RSH_CMD=ssh -F /cygdrive/c/Users/biocbuild/.ssh/config
+set BBS_RSH_CMD=ssh -F /cygdrive/f/biocbuild/.ssh/config
 set BBS_CENTRAL_ROOT_URL=http://155.52.207.166
 
 
