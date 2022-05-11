f0751659ec5d530a6594cdeb1eb097561e6b0861
Hervé Pagès <hpages.on.github@gmail.com>
2022-05-10 20:39:02 -0700

    Wait 1 min before the final 'kill -9 -1' on Linux and Mac

--- 3.15/bioc/merida1/run.sh
+++ 3.15/bioc/merida1/run.sh
@@ -8,4 +8,5 @@ $BBS_PYTHON_CMD $BBS_HOME/BBS-run.py
 
 # We have to use brute force because some R processes might still be running
 # in the background. This will kick out the user BBS is running as!
+sleep 60  # wait 1 min before the kill
 kill -9 -1
