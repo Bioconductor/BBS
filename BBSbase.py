086e161b91de67a3332cb81b0dabe103a021e6cc
Hervé Pagès <hpages.on.github@gmail.com>
2022-10-24 14:59:59 -0700

    Drop --no-vignettes when doing CHECK on Linux

--- BBSbase.py
+++ BBSbase.py
@@ -435,7 +435,11 @@ def getSTAGE4cmd(srcpkg_path):
                         "--ignore-vignettes",
                         "--check-subdirs=no"]
     else:
-        common_opts += ["--no-vignettes", "--timings"]
+        ## Skip the vignette checks on Windows and Mac only, to reduce
+        ## build times.
+        if sys.platform in ['win32', 'darwin']:
+            common_opts += ["--no-vignettes"]
+        common_opts += ["--timings"]
         ## Note that sys.platform is set to 'win32' on 64-bit Windows. This
         ## means that _noExampleArchs() may not be returning useful results
         ## if the intent is to not run examples for a particular Windows
