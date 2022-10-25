12ae82010518216165db434cdec7d52029034175
J Wokaty <jwokaty@users.noreply.github.com>
2022-10-25 16:19:34 -0400

    Add 3.17 configuration files

---
+++ 3.17/workflows/merida1/config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# ====================
+# Settings for merida1
+# ====================
+
+
+
+#set -x  # print commands and their arguments as they are executed
+
+export BBS_DEBUG="0"
+
+export BBS_NODE_HOSTNAME="merida1"
+export BBS_USER="biocbuild"
+export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.17-workflows"
+export BBS_R_HOME="/Library/Frameworks/R.framework/Resources"
+export BBS_NB_CPU=16  # 48 cores are available
+
+# Central build node is nebbiolo1 at DFCI.
+export BBS_CENTRAL_RHOST="nebbiolo1"
+export BBS_RSH_CMD="ssh -F /Users/biocbuild/.ssh/config"
+export BBS_CENTRAL_ROOT_URL="http://155.52.207.165"
+
+# Source tarballs produced during STAGE3 won't be propagated
+# so we don't need to push them to the central builder.
+export DONT_PUSH_SRCPKGS="1"
+
+
+
+# Shared settings (by all Unix nodes).
+
+wd0=$(pwd)
+cd ..
+. ./config.sh
+cd "$wd0"
