e0b5ffaf7888626ffd5d37934f1869b464c1fda0
J Wokaty <jwokaty@users.noreply.github.com>
2023-04-24 13:37:17 -0400

    Add 3.18 propagation

---
+++ propagation/3.18/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# =========================================================
+# Settings shared by all scripts in propagation/3.18 folder
+# =========================================================
+
+BIOC_VERSION="3.18"
+R_VERSION="4.3"
+Rscript="$HOME/bin/Rscript-$R_VERSION"
---
+++ propagation/3.18/prepareRepos-data-experiment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd "$HOME/propagation/3.18"
+
+. ./config.sh
+. ./prepareRepos-data-experiment.config
+. ./prepareRepos.sh
+
