532e25ea1b6c4f5bbca7f5a3a68798bd6f445d73
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2023-06-05 22:18:02 +0300

    Issue #292: Move kunpeng2 specific env vars to its local-settings.sh
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- 3.18/bioc/kunpeng2/config.sh
+++ 3.18/bioc/kunpeng2/config.sh
@@ -3,17 +3,6 @@
 # Settings for kunpeng2 (Linux ARM64)
 # ===================================
 
-# Specific for openEuler START because some libraries are installed from source
-export LIBSBML_CFLAGS=$(pkg-config --cflags ~/libsbml-from-git/lib/pkgconfig/libsbml.pc)
-export LIBSBML_LIBS=$(pkg-config --libs ~/libsbml-from-git/lib/pkgconfig/libsbml.pc)
-export UDUNITS2_INCLUDE="/home/biocbuild/libudunits-2/include"
-export UDUNITS2_LIBS="/home/biocbuild/libudunits-2/lib"
-export OPEN_BABEL_HOME="/home/biocbuild/openbabel-3.1.1"
-export OPENBABEL_CFLAGS="-I$OPEN_BABEL_HOME/include/openbabel3 -L$OPEN_BABEL_HOME/lib"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64:$UDUNITS2_LIBS:/home/biocbuild/libsbml-from-git/lib:$OPEN_BABEL_HOME/lib
-export PATH=$PATH:/usr/lib64/openmpi/bin:/home/biocbuild/libudunits-2/bin:$HOME/.dotnet:$OPEN_BABEL_HOME/bin
-# Settings specific for openEuler END
-
 #set -x  # print commands and their arguments as they are executed
 
 export BBS_DEBUG="0"
--- nodes/kunpeng2/local-settings.sh
+++ nodes/kunpeng2/local-settings.sh
@@ -19,3 +19,14 @@ export BBS_RSYNC_CMD="/usr/bin/rsync"
 export BBS_TAR_CMD="/usr/bin/tar"
 export BBS_GIT_CMD="/usr/bin/git"
 export BBS_CURL_CMD="/usr/bin/curl"
+
+
+# We need these because some of the libraries/dependecies are installed from source
+export LIBSBML_CFLAGS=$(pkg-config --cflags ~/libsbml-from-git/lib/pkgconfig/libsbml.pc)
+export LIBSBML_LIBS=$(pkg-config --libs ~/libsbml-from-git/lib/pkgconfig/libsbml.pc)
+export UDUNITS2_INCLUDE="/home/biocbuild/libudunits-2/include"
+export UDUNITS2_LIBS="/home/biocbuild/libudunits-2/lib"
+export OPEN_BABEL_HOME="/home/biocbuild/openbabel-3.1.1"
+export OPENBABEL_CFLAGS="-I$OPEN_BABEL_HOME/include/openbabel3 -L$OPEN_BABEL_HOME/lib"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib64:$UDUNITS2_LIBS:/home/biocbuild/libsbml-from-git/lib:$OPEN_BABEL_HOME/lib
+export PATH=$PATH:/usr/lib64/openmpi/bin:/home/biocbuild/libudunits-2/bin:$HOME/.dotnet:$OPEN_BABEL_HOME/bin
