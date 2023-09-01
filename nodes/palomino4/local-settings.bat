c69768811dd3c027ad305ce70826b765a80b6098
J Wokaty <jwokaty@users.noreply.github.com>
2023-09-01 15:46:17 -0400

    Update Python on Windows to 3.11

--- nodes/palomino4/local-settings.bat
+++ nodes/palomino4/local-settings.bat
@@ -5,7 +5,7 @@
 
 set BBS_HOME=F:\biocbuild\BBS
 
-set BBS_PYTHON_CMD=C:\Python39\python.exe
+set BBS_PYTHON_CMD=C:\Python311\python.exe
 
 set BBS_CURL_CMD=C:\cygwin\bin\curl.exe
 set BBS_SSH_CMD=C:\cygwin\bin\ssh.exe
