1da60fee86bf09f2d0ec2c2c2014af486f4a77c4
jwokaty <jwokaty@users.noreply.github.com>
2023-10-20 11:31:23 -0400

    Add 3.19 configuration files (#350)
    * Add 3.19 configuration files

---
+++ 3.19/bioc-mac-arm64/nebbiolo1/config.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# ======================
+# Settings for nebbiolo1
+# ======================
+
+
+
+#set -x  # print commands and their arguments as they are executed
+
+export BBS_DEBUG="0"
+
+export BBS_NODE_HOSTNAME="nebbiolo1"
+export BBS_USER="biocbuild"
+export BBS_WORK_TOPDIR="/home/biocbuild/bbs-3.19-bioc-mac-arm64"
+export BBS_R_HOME="/home/biocbuild/bbs-3.19-bioc/R"
+export R_LIBS="$BBS_R_HOME/site-library"
+export BBS_NB_CPU=24        # 72 cores are available
+export BBS_CHECK_NB_CPU=30  # 72 cores are available
+
+export BBS_CENTRAL_RHOST="localhost"
+export BBS_CENTRAL_ROOT_URL="http://$BBS_CENTRAL_RHOST"
+
+
+
+# Shared settings (by all Unix nodes).
+
+wd0=$(pwd)
+cd ..
+. ./config.sh
+cd "$wd0"
+
+
+
+# -----------------------------------------------------------------------------
+# The variables below control postrun.sh so only need to be defined on the
+# central node
+
+# Control propagation:
+export BBS_OUTGOING_MAP="mac.binary.big-sur-arm64:kjohnson2/buildbin"
+export BBS_FINAL_REPO="file://home/biocpush/PACKAGES/$BBS_BIOC_VERSION/bioc"
+
+# Control generation of the report:
+export BBS_REPORT_NODES="kjohnson2:bin"
+export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
+export BBS_REPORT_CSS="$BBS_HOME/$BBS_BIOC_VERSION/report.css"
+export BBS_REPORT_BGIMG="$BBS_HOME/images/DEVEL3b.png"
+export BBS_REPORT_JS="$BBS_HOME/$BBS_BIOC_VERSION/report.js"
+#export BBS_REPORT_MOTD="Happy new year to all Bioconductor developers!"
+
+# Control where to publish the report:
+export BBS_PUBLISHED_REPORT_RELATIVEURL="checkResults/$BBS_BIOC_VERSION/$BBS_BUILDTYPE-LATEST/"
+export BBS_PUBLISHED_REPORT_DEST_DIR="webadmin@master.bioconductor.org:/extra/www/bioc/$BBS_PUBLISHED_REPORT_RELATIVEURL"
+
+
+# -----------------------------------------------------------------------------
+# The variables below control stage7-notify.sh so only need to be defined on
+# the central node
+
+# TODO: when BBS_NOTIFY_NODES is not defined then take all the build nodes
+export BBS_NOTIFY_NODES="nebbiolo1"
+export BBS_PUBLISHED_REPORT_URL="https://master.bioconductor.org/$BBS_PUBLISHED_REPORT_RELATIVEURL"
+
