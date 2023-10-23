31b0af8820fca54ff5755e373d11c9bc47fa5b3d
J Wokaty <jwokaty@users.noreply.github.com>
2023-10-23 09:57:16 -0400

    Add 3.19 propagation

---
+++ propagation/3.19/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# =========================================================
+# Settings shared by all scripts in propagation/3.19 folder
+# =========================================================
+
+BIOC_VERSION="3.19"
+R_VERSION="4.3"
+Rscript="$HOME/bin/Rscript-$R_VERSION"
---
+++ propagation/3.19/prepareRepos-workflows.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd "$HOME/propagation/3.19"
+
+. ./config.sh
+. ./prepareRepos-workflows.config
+. ./prepareRepos.sh
+
