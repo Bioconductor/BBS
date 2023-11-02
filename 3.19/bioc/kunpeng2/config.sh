b64905e31021bf1582453c0bcb7b94e8edff7b80
Martin Grigorov <martin-g@users.noreply.github.com>
2023-11-02 20:53:01 +0200

    Add config for kunpeng2 (Linux ARM64 builder) for 3.19 (#358)
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

---
+++ 3.19/bioc/kunpeng2/config.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# ===================================
+# Settings for kunpeng2 (Linux ARM64)
+# ===================================
+
+#set -x  # print commands and their arguments as they are executed
+
+export BBS_DEBUG="0"
+
+export BBS_NODE_HOSTNAME="kunpeng2"
+export BBS_USER="biocbuild"
+export BBS_WORK_TOPDIR="/home/biocbuild/bbs-3.19-bioc"
+export BBS_R_HOME="/home/biocbuild/R/R-4.4-devel-2023.11.02"
+export R_LIBS="$BBS_R_HOME/site-library"
+export BBS_NB_CPU=25         # 32 cores are available
+export BBS_BUILD_NB_CPU=16   # 32 cores are available
+export BBS_CHECK_NB_CPU=22   # 32 cores are available
+export BBS_EXTRA_CHECK_OPTIONS="--no-vignettes"
+
+export BBS_PRODUCT_TRANSMISSION_MODE="none"
+
+# Central build node is nebbiolo1 at DFCI.
+export BBS_CENTRAL_ROOT_URL="http://155.52.207.165"
+
+# Source tarballs produced during STAGE3 won't be propagated so
+# we don't need to make them available to the central builder.
+export DONT_PUSH_SRCPKGS="1"
+
+# Shared settings (by all Unix nodes).
+
+wd0=$(pwd)
+cd ..
+. ./config.sh
+cd "$wd0"
