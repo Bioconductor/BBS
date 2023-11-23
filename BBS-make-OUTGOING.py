8759d0bec279372eae22d17ca8d6cb92bb125f20
Hervé Pagès <hpages.on.github@gmail.com>
2023-11-22 20:01:06 -0800

    more tweaks

--- BBS-make-OUTGOING.py
+++ BBS-make-OUTGOING.py
@@ -4,7 +4,7 @@
 ### This file is part of the BBS software (Bioconductor Build System).
 ###
 ### Author: Hervé Pagès <hpages.on.github@gmail.com>
-### Last modification: Oct 6, 2020
+### Last modification: Nov 22, 2023
 ###
 
 import sys
@@ -20,12 +20,12 @@ import BBSvars
 import BBSbase
 
 def is_doing_buildbin(node_hostname):
-    return BBSutils.getNodeSpec(node_hostname, 'pkgType') != "source"
+    return BBSutils.getNodeSpec(node_hostname, 'pkgType') != 'source'
 
 def block_package(node_hostname, node_id, pkg):
     nodes_path = BBSvars.products_in_rdir.path
     node_path = os.path.join(nodes_path, node_id)
-    summary_file0 = "%s.%%s-summary.dcf" % pkg
+    summary_file0 = '%s.%%s-summary.dcf' % pkg
 
     ## Extract Status from BUILD summary.
     buildsrc_path = os.path.join(node_path, 'buildsrc')
@@ -42,8 +42,8 @@ def block_package(node_hostname, node_id, pkg):
     if status != 'OK':
         return True
 
-    ## workflows and books buildtype exit here
-    if BBSvars.buildtype in ["workflows", "books"]:
+    ## Not going further for workflows builds.
+    if BBSvars.buildtype == 'workflows':
         return status != 'OK'
 
     ## Extract Status from CHECK summary.
@@ -55,7 +55,7 @@ def block_package(node_hostname, node_id, pkg):
         return True
     status = bbs.parse.get_next_DCF_val(dcf, 'Status')
     dcf.close()
-    if status not in ["OK", "WARNINGS"]:
+    if status not in ['OK', 'WARNINGS']:
         return True
 
     if not is_doing_buildbin(node_hostname):
@@ -74,11 +74,11 @@ def block_package(node_hostname, node_id, pkg):
 
 ### Copy the outgoing packages to the current directory.
 def copy_outgoing_pkgs(products_in_subdir, source_node):
-    tmp = products_in_subdir.split("/")
+    tmp = products_in_subdir.split('/')
     if len(tmp) != 2:
-        sys.exit("ERROR: Invalid relative path to fresh pkgs %s (must be of the form node/subdir)" % products_in_subdir)
+        sys.exit('ERROR: Invalid relative path to fresh pkgs %s (must be of the form node/subdir)' % products_in_subdir)
     node_id = tmp[0]
-    node_hostname = node_id.split("-")[0]
+    node_hostname = node_id.split('-')[0]
     fileext = BBSutils.getNodeSpec(node_hostname, 'pkgFileExt')
     srcdir  = os.path.join(BBSvars.products_in_rdir.path, products_in_subdir)
     if not os.path.exists(srcdir):
@@ -87,13 +87,13 @@ def copy_outgoing_pkgs(products_in_subdir, source_node):
         raise FileExistsError(errmsg)
     ## Workflow and book packages do not have manuals/ because we do not run
     ## `R CMD check`.
-    manuals_dir = "../manuals"
-    if BBSvars.buildtype in ["workflows", "books", "bioc-mac-arm64"]:
+    manuals_dir = '../manuals'
+    if BBSvars.buildtype in ['workflows', 'books', 'bioc-mac-arm64']:
         pass
     elif source_node:
-        print("BBS> [stage6b] mkdir %s" % manuals_dir)
+        print('BBS> [stage6b] mkdir %s' % manuals_dir)
         os.mkdir(manuals_dir)
-    print("BBS> [stage6b] BEGIN copying outgoing packages from %s." % srcdir)
+    print('BBS> [stage6b] BEGIN copying outgoing packages from %s.' % srcdir)
     node_Arch = BBSutils.getNodeSpec(node_hostname, 'Arch')
     node_pkgType = BBSutils.getNodeSpec(node_hostname, 'pkgType')
     meat_index_path = os.path.join(BBSvars.Central_rdir.path,
@@ -107,51 +107,51 @@ def copy_outgoing_pkgs(products_in_subdir, source_node):
         dcf_record = meat_index[pkg]
         version = dcf_record['Version']
         ## Copy pkg from 'srcdir'.
-        pkg_file = "%s_%s.%s" % (pkg, version, fileext)
+        pkg_file = '%s_%s.%s' % (pkg, version, fileext)
         pkg_path = os.path.join(srcdir, pkg_file)
-        print("BBS> [stage6b]   - copying %s to OUTGOING folder ..." % pkg_path)
+        print('BBS> [stage6b]   - copying %s to OUTGOING folder ...' % pkg_path)
         if os.path.exists(pkg_path):
-            #shutil.copy(pkg_path, ".")
+            #shutil.copy(pkg_path, '.')
             os.link(pkg_path, pkg_file)  # create hard link to avoid making a copy
         else:
             print("BBS> [stage6b]     SKIPPED (file %s doesn't exist)" % pkg_path)
         ## Get reference manual from pkg.Rcheck directory.
-        if BBSvars.buildtype in ["workflows", "books", "bioc-mac-arm64"]:
+        if BBSvars.buildtype in ['workflows', 'books', 'bioc-mac-arm64']:
             pass
         elif source_node:
             pdf_file = os.path.join(BBSutils.getenv('BBS_WORK_TOPDIR'),
-                                    "meat",
-                                    "%s.Rcheck" % pkg,
-                                    "%s-manual.pdf" % pkg)
-            print("BBS> [stage6b]   - copying %s manual to OUTGOING/manuals folder..." % pkg)
+                                    'meat',
+                                    '%s.Rcheck' % pkg,
+                                    '%s-manual.pdf' % pkg)
+            print('BBS> [stage6b]   - copying %s manual to OUTGOING/manuals folder...' % pkg)
             if os.path.exists(pdf_file):
-                dst = os.path.join(manuals_dir, "%s.pdf" % pkg)
+                dst = os.path.join(manuals_dir, '%s.pdf' % pkg)
                 #shutil.copy(pdf_file, dst)
                 os.link(pdf_file, dst) # create hard link to avoid making a copy
             else:
                 print("BBS> [stage6b]     SKIPPED (file %s doesn't exist)" % pdf_file)
-    print("BBS> [stage6b] END copying outgoing packages from %s." % srcdir)
+    print('BBS> [stage6b] END copying outgoing packages from %s.' % srcdir)
     return
 
 def stage6_make_OUTGOING():
     ## Create working directory
-    OUTGOING_dir = os.path.join(BBSvars.Central_rdir.path, "OUTGOING")
-    print("BBS> [stage6b] remake_dir %s" % OUTGOING_dir)
+    OUTGOING_dir = os.path.join(BBSvars.Central_rdir.path, 'OUTGOING')
+    print('BBS> [stage6b] remake_dir %s' % OUTGOING_dir)
     bbs.fileutils.remake_dir(OUTGOING_dir)
     ## Loop over each element of the OUTGOING map
     OUTGOING_MAP = BBSutils.getenv('BBS_OUTGOING_MAP')
-    map_elts = OUTGOING_MAP.split(" ")
+    map_elts = OUTGOING_MAP.split(' ')
     for map_elt in map_elts:
-        tmp = map_elt.split(":")
+        tmp = map_elt.split(':')
         if len(tmp) != 2:
-            sys.exit("ERROR: Invalid OUTGOING map element %s" % map_elt)
+            sys.exit('ERROR: Invalid OUTGOING map element %s' % map_elt)
         source_node = False
-        if tmp[0] == "source":
+        if tmp[0] == 'source':
             source_node = True
         OUTGOING_subdir = os.path.join(OUTGOING_dir, tmp[0])
-        print("BBS> [stage6b] mkdir %s" % OUTGOING_subdir)
+        print('BBS> [stage6b] mkdir %s' % OUTGOING_subdir)
         os.mkdir(OUTGOING_subdir)
-        print("BBS> [stage6b] cd %s/" % OUTGOING_subdir)
+        print('BBS> [stage6b] cd %s/' % OUTGOING_subdir)
         os.chdir(OUTGOING_subdir)
         try:
             copy_outgoing_pkgs(tmp[1], source_node)
@@ -167,11 +167,11 @@ def stage6_make_OUTGOING():
 
 
 print()
-print("BBS> ==================================================================")
-print("BBS> [stage6b] STARTING stage6b on %s..." % time.asctime())
+print('BBS> ==================================================================')
+print('BBS> [stage6b] STARTING stage6b on %s...' % time.asctime())
 sys.stdout.flush()
 
 stage6_make_OUTGOING()
 
-print("BBS> [stage6b] DONE on %s." % time.asctime())
+print('BBS> [stage6b] DONE on %s.' % time.asctime())
 
