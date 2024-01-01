6e9744bd27a04021b6c3f6312333af4cc2518d06
jwokaty <jwokaty@users.noreply.github.com>
2024-01-01 14:37:12 -0500

    Use default python3 for merida1 (#385)

--- nodes/merida1/local-settings.sh
+++ nodes/merida1/local-settings.sh
@@ -8,7 +8,7 @@ if [ -z "$BBS_HOME" ]; then
     export BBS_HOME="/Users/biocbuild/BBS"
 fi
 
-export BBS_PYTHON_CMD="/usr/local/bin/python3"
+export BBS_PYTHON_CMD="/usr/bin/python3"
 
 export BBS_CURL_CMD="/usr/bin/curl"
 export BBS_SSH_CMD="/usr/bin/ssh"
