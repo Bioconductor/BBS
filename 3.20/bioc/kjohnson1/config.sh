1781f2c6bde6908585498f641bd5e5fae3fc7ccb
Hervé Pagès <hpages.on.github@gmail.com>
2024-05-28 19:01:11 -0700

    replace kjohnson3 with kjohnson1 in the 3.20/bioc builds

---
+++ 3.20/bioc/kjohnson1/config.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# ======================
+# Settings for kjohnson1
+# ======================
+
+
+
+#set -x  # print commands and their arguments as they are executed
+
+export BBS_DEBUG="0"
+
+export BBS_NODE_HOSTNAME="kjohnson1"
+export BBS_USER="biocbuild"
+export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.20-bioc"
+export BBS_R_HOME="/Library/Frameworks/R.framework/Resources"
+
+# kjohnson1 has 10 cores available.
+export BBS_NB_CPU=7
+export BBS_BUILD_NB_CPU=5
+export BBS_CHECK_NB_CPU=6
+export BBS_EXTRA_CHECK_OPTIONS="--no-vignettes"
+
+# Central build node is bbscentral1 on Jetstream2.
+export BBS_CENTRAL_RHOST="bbscentral1"
+export BBS_RSH_CMD="ssh -F /Users/biocbuild/.ssh/config"
+export BBS_CENTRAL_ROOT_URL="http://149.165.171.124"
+export BBS_PRODUCT_TRANSMISSION_MODE="asynchronous"
+
+# Source tarballs produced during STAGE3 won't be propagated
+# so we don't need to push them to the central builder.
+export DONT_PUSH_SRCPKGS="1"
+
+
+
+# Shared settings (by all Unix nodes).
+
+wd0=$(pwd)
+cd ..
+. ./config.sh
+cd "$wd0"
