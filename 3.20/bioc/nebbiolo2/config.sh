34bb841366dc5f8f8a3a3e1ebd84c98c8e10f2c6
Hervé Pagès <hpages.on.github@gmail.com>
2024-05-30 10:06:56 -0700

    All 3.20/bioc builders also switched from bbscentral1 to bbscentral2 to use as central node

--- 3.20/bioc/nebbiolo2/config.sh
+++ 3.20/bioc/nebbiolo2/config.sh
@@ -20,10 +20,10 @@ export BBS_NB_CPU=18
 export BBS_BUILD_NB_CPU=15
 export BBS_CHECK_NB_CPU=22
 
-# Central build node is bbscentral1 on Jetstream2.
-export BBS_CENTRAL_RHOST="bbscentral1"
+# Central build node is bbscentral2 on Jetstream2.
+export BBS_CENTRAL_RHOST="bbscentral2"
 export BBS_RSH_CMD="ssh -F /home/biocbuild/.ssh/config"
-export BBS_CENTRAL_ROOT_URL="http://149.165.171.124"
+export BBS_CENTRAL_ROOT_URL="http://149.165.154.78"
 export BBS_PRODUCT_TRANSMISSION_MODE="asynchronous"
 
 
