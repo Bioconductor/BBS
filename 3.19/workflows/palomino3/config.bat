88f06f343279a892b7d5c00fbe498e3ebbd80d03
Hervé Pagès <hpages.on.github@gmail.com>
2024-05-30 18:30:25 -0700

    All 3.19/workflows builders now using bbscentral1 as central node

--- 3.19/workflows/palomino3/config.bat
+++ 3.19/workflows/palomino3/config.bat
@@ -1,6 +1,6 @@
-@rem ===================
+@rem ======================
 @rem Settings for palomino3
-@rem ===================
+@rem ======================
 
 
 set BBS_DEBUG=0
@@ -14,10 +14,11 @@ set BBS_R_HOME=F:\biocbuild\bbs-3.19-bioc\R
 set BBS_NB_CPU=10
 set BBS_BUILD_NB_CPU=8
 
-@rem Central build node is nebbiolo1 at DFCI.
-set BBS_CENTRAL_RHOST=nebbiolo1
+@rem Central build node is bbscentral1 on Jetstream2.
+set BBS_CENTRAL_RHOST=bbscentral1
 set BBS_RSH_CMD=ssh -F /cygdrive/f/biocbuild/.ssh/config
-set BBS_CENTRAL_ROOT_URL=http://155.52.207.165
+set BBS_CENTRAL_ROOT_URL=http://149.165.171.124
+set BBS_PRODUCT_TRANSMISSION_MODE=asynchronous
 
 @rem Source tarballs produced during STAGE3 won't be propagated
 @rem so we don't need to push them to the central builder.
@@ -25,7 +26,7 @@ set DONT_PUSH_SRCPKGS=1
 
 
 
-@rem Shared settings (by all Windows nodes)
+@rem Shared settings (by all Windows nodes).
 
 set wd0=%cd%
 cd ..
