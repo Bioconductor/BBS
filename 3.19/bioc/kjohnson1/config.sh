a576e88c2ed73a9808b7efae9e88b88940817776
J Wokaty <jwokaty@users.noreply.github.com>
2024-06-12 08:36:27 -0400

    Switch kjohnson3 kjohnson1

--- 3.19/bioc/kjohnson1/config.sh
+++ 3.19/bioc/kjohnson1/config.sh
@@ -11,7 +11,7 @@ export BBS_DEBUG="0"
 
 export BBS_NODE_HOSTNAME="kjohnson1"
 export BBS_USER="biocbuild"
-export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.20-bioc"
+export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.19-bioc"
 export BBS_R_HOME="/Library/Frameworks/R.framework/Resources"
 
 # kjohnson1 has 10 logical CPUs.
@@ -20,10 +20,10 @@ export BBS_BUILD_NB_CPU=5
 export BBS_CHECK_NB_CPU=6
 export BBS_EXTRA_CHECK_OPTIONS="--no-vignettes"
 
-# Central build node is bbscentral2 on Jetstream2.
-export BBS_CENTRAL_RHOST="bbscentral2"
+# Central build node is bbscentral1 on Jetstream2.
+export BBS_CENTRAL_RHOST="bbscentral1"
 export BBS_RSH_CMD="ssh -F /Users/biocbuild/.ssh/config"
-export BBS_CENTRAL_ROOT_URL="http://149.165.154.78"
+export BBS_CENTRAL_ROOT_URL="http://149.165.171.124"
 export BBS_PRODUCT_TRANSMISSION_MODE="asynchronous"
 
 # Source tarballs produced during STAGE3 won't be propagated
--- 3.20/bioc/kjohnson3/config.sh
+++ 3.20/bioc/kjohnson3/config.sh
@@ -11,7 +11,7 @@ export BBS_DEBUG="0"
 
 export BBS_NODE_HOSTNAME="kjohnson3"
 export BBS_USER="biocbuild"
-export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.19-bioc"
+export BBS_WORK_TOPDIR="/Users/biocbuild/bbs-3.20-bioc"
 export BBS_R_HOME="/Library/Frameworks/R.framework/Resources"
 
 # kjohnson3 has 24 logical CPUs (16 performance, 8 efficiency).
@@ -20,10 +20,10 @@ export BBS_BUILD_NB_CPU=8
 export BBS_CHECK_NB_CPU=9
 export BBS_EXTRA_CHECK_OPTIONS="--no-vignettes"
 
-# Central build node is bbscentral1 on Jetstream2.
-export BBS_CENTRAL_RHOST="bbscentral1"
+# Central build node is bbscentral2 on Jetstream2.
+export BBS_CENTRAL_RHOST="bbscentral2"
 export BBS_RSH_CMD="ssh -F /Users/biocbuild/.ssh/config"
-export BBS_CENTRAL_ROOT_URL="http://149.165.171.124"
+export BBS_CENTRAL_ROOT_URL="http://149.165.154.78"
 export BBS_PRODUCT_TRANSMISSION_MODE="asynchronous"
 
 # Source tarballs produced during STAGE3 won't be propagated
