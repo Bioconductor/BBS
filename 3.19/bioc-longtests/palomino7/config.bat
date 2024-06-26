f8f35f9377c83086dbcef427d102a068f6d3a1ed
jwokaty <jwokaty@users.noreply.github.com>
2024-06-26 10:30:12 -0400

    Add palomino7 (#412)
    * Add palomino7
    
    * Add palomino7 to 3.19 longtests and workflows

---
+++ 3.19/bioc-longtests/palomino7/config.bat
@@ -0,0 +1,29 @@
+@rem ======================
+@rem Settings for palomino7
+@rem ======================
+
+
+set BBS_DEBUG=0
+
+set BBS_NODE_HOSTNAME=palomino7
+set BBS_USER=biocbuild
+set BBS_WORK_TOPDIR=F:\biocbuild\bbs-3.19-bioc-longtests
+set BBS_R_HOME=F:\biocbuild\bbs-3.19-bioc\R
+
+@rem palomino7 has 32 logical CPUs.
+set BBS_NB_CPU=8
+
+@rem Central build node is bbscentral1 on Jetstream2.
+set BBS_CENTRAL_RHOST=bbscentral1
+set BBS_RSH_CMD=ssh -F /cygdrive/f/biocbuild/.ssh/config
+set BBS_CENTRAL_ROOT_URL=http://149.165.171.124
+set BBS_PRODUCT_TRANSMISSION_MODE=asynchronous
+
+
+
+@rem Shared settings (by all Windows nodes).
+
+set wd0=%cd%
+cd ..
+call config.bat
+cd %wd0%
