b692b7dd14f117ea9f538a8cc1b9a5c2afa58b6c
J Wokaty <jwokaty@users.noreply.github.com>
2024-07-10 22:16:29 -0400

    Fix palomino8 config path

--- 3.20/workflows/palomino8/config.bat
+++ 3.20/workflows/palomino8/config.bat
@@ -16,7 +16,7 @@ set BBS_BUILD_NB_CPU=8
 
 @rem Central build node is bbscentral2 on Jetstream2.
 set BBS_CENTRAL_RHOST=bbscentral2
-set BBS_RSH_CMD=ssh -F /cygdrive/f/Users/biocbuild/.ssh/config
+set BBS_RSH_CMD=ssh -F /cygdrive/f/biocbuild/.ssh/config
 set BBS_CENTRAL_ROOT_URL=http://149.165.154.78
 set BBS_PRODUCT_TRANSMISSION_MODE=asynchronous
 
