8fd35c74e2de85cbb801aca98e9df5846f139b22
J Wokaty <jwokaty@users.noreply.github.com>
2024-07-17 10:21:42 -0400

    Kill R processes on kjohnson3

--- 3.20/bioc/kjohnson3/run.sh
+++ 3.20/bioc/kjohnson3/run.sh
@@ -9,4 +9,4 @@ $BBS_PYTHON_CMD $BBS_HOME/BBS-run.py
 # We have to use brute force because some R processes might still be running
 # in the background. This will kick out the user BBS is running as!
 sleep 60  # wait 1 min before the kill
-kill -9 -1
+pkill -9 -f /Library/Frameworks/R.framework/Resources/bin/exec/R
