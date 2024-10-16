7fffe1caaa4c344854e1123bfb667d138171b7b5
J Wokaty <jwokaty@users.noreply.github.com>
2024-10-16 16:10:05 -0400

    Copy 3.20 to 3.21

---
+++ 3.21/config.bat
@@ -0,0 +1,37 @@
+@rem ====================================================================
+@rem Settings shared by all the Windows nodes involved in the 3.20 builds
+@rem ====================================================================
+
+
+@rem Paths to local commands
+@rem -----------------------
+
+call ..\nodes\%BBS_NODE_HOSTNAME%\local-settings.bat
+
+set BBS_RSYNC_RSH_CMD=%BBS_RSYNC_CMD% --rsh '%BBS_RSH_CMD%'
+set BBS_RSYNC_OPTIONS=-r --delete --delete-excluded --exclude='.svn' --exclude='.git' --exclude='.github' --exclude='.git_*'
+
+set BBS_R_CMD=%BBS_R_HOME%\bin\R.exe
+set BBS_RSCRIPT_CMD=%BBS_R_HOME%\bin\Rscript.exe
+set TMP=%BBS_WORK_TOPDIR%\tmp
+set TMPDIR=%BBS_WORK_TOPDIR%\tmpdir
+
+
+@rem Variables specifying the version and mode of the current builds
+@rem ---------------------------------------------------------------
+
+set BBS_BIOC_VERSION=3.20
+set BBS_BIOC_GIT_BRANCH=devel
+set BBS_BIOC_VERSIONED_REPO_PATH=%BBS_BIOC_VERSION%/%BBS_BUILDTYPE%
+set BBS_NON_TARGET_REPOS_FILE=%BBS_HOME%/%BBS_BIOC_VERSIONED_REPO_PATH%/non_target_repos.txt
+
+set BBS_CENTRAL_RUSER=biocbuild
+set BBS_CENTRAL_RDIR=/home/biocbuild/public_html/BBS/%BBS_BIOC_VERSIONED_REPO_PATH%
+set BBS_CENTRAL_BASEURL=%BBS_CENTRAL_ROOT_URL%/BBS/%BBS_BIOC_VERSIONED_REPO_PATH%
+
+
+@rem Define some environment variables to control the behavior of R
+@rem --------------------------------------------------------------
+
+set R_ENVIRON_USER=%BBS_HOME%\%BBS_BIOC_VERSION%\Renviron.bioc
+
---
+++ 3.21/workflows/palomino8/config.bat
@@ -0,0 +1,34 @@
+@rem ======================
+@rem Settings for palomino8
+@rem ======================
+
+
+set BBS_DEBUG=0
+
+set BBS_NODE_HOSTNAME=palomino8
+set BBS_USER=biocbuild
+set BBS_WORK_TOPDIR=F:\biocbuild\bbs-3.20-workflows
+set BBS_R_HOME=F:\biocbuild\bbs-3.20-bioc\R
+
+@rem palomino8 has 32 logical CPUs.
+set BBS_NB_CPU=10
+set BBS_BUILD_NB_CPU=8
+
+@rem Central build node is bbscentral2 on Jetstream2.
+set BBS_CENTRAL_RHOST=bbscentral2
+set BBS_RSH_CMD=ssh -F /cygdrive/f/biocbuild/.ssh/config
+set BBS_CENTRAL_ROOT_URL=http://149.165.152.87
+set BBS_PRODUCT_TRANSMISSION_MODE=asynchronous
+
+@rem Source tarballs produced during STAGE3 won't be propagated
+@rem so we don't need to push them to the central builder.
+set DONT_PUSH_SRCPKGS=1
+
+
+
+@rem Shared settings (by all Windows nodes).
+
+set wd0=%cd%
+cd ..
+call config.bat
+cd %wd0%
