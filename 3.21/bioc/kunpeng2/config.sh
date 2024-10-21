0cd69c9e9be73a3b45d58694f8e889b5e91254cb
jwokaty <jwokaty@users.noreply.github.com>
2024-10-21 15:59:57 -0400

    Issue/426 (#427)
    * Copy 3.20 to 3.21
    * Rename 3.20 to 3.21
    * Rename R 4.4 to R 4.5
    * Remove mac arm64 builds
    * Rename machines, remove teran2
    * No propagation, only nebbiolo1 on bioc report
    * Add strict r headers and no remap to Renviron
    * Add body color to css

---
+++ 3.21/bioc/kunpeng2/config.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# ===================================
+# Settings for kunpeng2 (Linux ARM64)
+# ===================================
+
+
+
+#set -x  # print commands and their arguments as they are executed
+
+export BBS_DEBUG="0"
+
+export BBS_NODE_HOSTNAME="kunpeng2"
+export BBS_USER="biocbuild"
+export BBS_WORK_TOPDIR="/home/biocbuild/bbs-3.21-bioc"
+export BBS_R_HOME="/home/biocbuild/R/R"
+export R_LIBS="$BBS_R_HOME/site-library"
+
+# kunpeng2 has 32 logical CPUs.
+export BBS_NB_CPU=25
+export BBS_BUILD_NB_CPU=16
+export BBS_CHECK_NB_CPU=22
+export BBS_EXTRA_CHECK_OPTIONS="--no-vignettes"
+
+# Central build node is bbscentral1 on Jetstream2.
+export BBS_CENTRAL_ROOT_URL="http://149.165.171.124"
+export BBS_PRODUCT_TRANSMISSION_MODE="none"
+
+# Source tarballs produced during STAGE3 won't be propagated
+# so we don't need to push them to the central builder.
+export DONT_PUSH_SRCPKGS="1"
+
+
+
+# Shared settings (by all Unix nodes).
+
+wd0=$(pwd)
+cd ..
+. ./config.sh
+cd "$wd0"
---
+++ 3.21/workflows/config.bat
@@ -0,0 +1,47 @@
+@rem ==============================================================================
+@rem Settings shared by all the Windows nodes involved in the 3.21-workflows builds
+@rem ==============================================================================
+
+
+set BBS_BUILDTYPE=workflows
+
+
+set wd1=%cd%
+cd ..
+call config.bat
+cd %wd1%
+
+
+@rem What type of meat? Only 3 types are supported:
+@rem   1: svn repo (contains pkg dirs)
+@rem   2: CRAN-style local repo containing .tar.gz pkgs
+@rem   3: git repo containing pkg dirs
+set BBS_MEAT0_TYPE=3
+
+@rem Needed only if BBS_MEAT0_TYPE is 3
+set BBS_BIOC_MANIFEST_GIT_REPO_URL=https://git.bioconductor.org/admin/manifest
+set BBS_BIOC_MANIFEST_GIT_BRANCH=devel
+set BBS_BIOC_MANIFEST_CLONE_PATH=%BBS_WORK_TOPDIR%\manifest
+
+@rem Needed if BBS_MEAT0_TYPE is 1 or 3
+set BBS_BIOC_MANIFEST_FILE=workflows.txt
+
+@rem Where is the fresh meat to be stored by prerun (stage1)
+set BBS_MEAT0_RHOST=%BBS_CENTRAL_RHOST%
+set BBS_MEAT0_RUSER=%BBS_CENTRAL_RUSER%
+set BBS_MEAT0_RDIR=/home/biocbuild/bbs-3.21-workflows/MEAT0
+
+@rem Triggers a MEAT0 update at beginning of prerun (stage1)
+set BBS_UPDATE_MEAT0=1
+
+@rem Local meat copy
+set BBS_MEAT_PATH=%BBS_WORK_TOPDIR%\meat
+
+@rem Where are the gitlog files stored by prerun (stage1)
+set BBS_GITLOG_RHOST=%BBS_CENTRAL_RHOST%
+set BBS_GITLOG_RUSER=%BBS_CENTRAL_RUSER%
+set BBS_GITLOG_RDIR=%BBS_CENTRAL_RDIR%/gitlog
+
+@rem Local gitlog copy
+set BBS_GITLOG_PATH=%BBS_WORK_TOPDIR%\gitlog
+
