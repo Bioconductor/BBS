c3011b405cae948d11adde91c6cef2238092b491
J Wokaty <jwokaty@users.noreply.github.com>
2025-02-10 12:51:57 -0500

    Add comments, copy html manuals for 3.20

--- BBS-make-meta-dbs.py
+++ BBS-make-meta-dbs.py
@@ -5,11 +5,11 @@ import os
 import time
 import subprocess
 
-import BBSutils
 import BBSvars
 import BBSbase
 
 def make_meta_dbs():
+    ## Following example of BBS-make-OUTGOING.py
     ## Prepare Rexpr (must be a single string with no spaces).
     Rscript_path = os.path.join(BBSvars.BBS_home,
                                 'utils',
@@ -20,14 +20,9 @@ def make_meta_dbs():
     Rfuncall = f"{Rfun}('{OUTGOING_dir}','{db_filepath}')"
     Rexpr = f"source('{Rscript_path}');{Rfuncall}"
 
-    ## Turn Rexpr into a system command.
     cmd = BBSbase.Rexpr2syscmd(Rexpr)
 
     try:
-        ## Nasty things (that I don't really understand) can happen with
-        ## subprocess.run() if this code is runned by the Task Scheduler
-        ## on Windows (the child process tends to almost always return an
-        ## error). Apparently using 'stderr=subprocess.STDOUT' fixes this pb.
         subprocess.run(cmd, stdout=None, stderr=subprocess.STDOUT, shell=True,
                        check=True)
     except subprocess.CalledProcessError as e:
