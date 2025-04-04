95d0c720fb2e20c2bf44a3fe705fbd0487d5dc0f
J Wokaty <jwokaty@users.noreply.github.com>
2025-04-04 10:59:33 -0400

    Add propagation/3.22

---
+++ propagation/3.22/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# =========================================================
+# Settings shared by all scripts in propagation/3.22 folder
+# =========================================================
+
+BIOC_VERSION="3.22"
+R_VERSION="4.5"
+Rscript="$HOME/bin/Rscript-$R_VERSION"
