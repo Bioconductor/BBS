87bf392ab20495a6ff34395905b0d4c58c179d59
A Wokaty <awokaty@users.noreply.github.com>
2025-05-07 18:55:18 -0400

    Add 3.22/bioc-gpu

---
+++ 3.22/bioc-gpu/biocgpu/config.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# ====================
+# Settings for biocgpu
+# ====================
+
+
+
+#set -x  # print commands and their arguments as they are executed
+
+export BBS_DEBUG="0"
+
+export BBS_NODE_HOSTNAME="biocgpu"
+export BBS_USER="biocbuild"
+export BBS_WORK_TOPDIR="/home/biocbuild/bbs-3.22-bioc-gpu"
+export BBS_R_HOME="$BBS_WORK_TOPDIR/R"
+export R_LIBS="$BBS_R_HOME/site-library"
+
+# biocgpu has 8 logical CPUs but only 1 GPU so for now we err on the side
+# of caution and set BBS_BUILD_NB_CPU and BBS_CHECK_NB_CPU to 1. This way
+# each package gets exclusive access to the GPU during 'R CMD build'
+# and 'R CMD check'.
+export BBS_NB_CPU=5
+export BBS_BUILD_NB_CPU=1
+export BBS_CHECK_NB_CPU=1
+
+export BBS_CENTRAL_RHOST="localhost"
+export BBS_CENTRAL_ROOT_URL="http://$BBS_CENTRAL_RHOST"
+
+
+
+# Shared settings (by all Unix nodes).
+
+wd0=$(pwd)
+cd ..
+. ./config.sh
+cd "$wd0"
+
+
+
+# -----------------------------------------------------------------------------
+# The variables below control postrun.sh so only need to be defined on the
+# central node
+
+# Control propagation:
+export BBS_OUTGOING_MAP="source:biocgpu/buildsrc"
+export BBS_FINAL_REPO="file://home/biocpush/PACKAGES/$BBS_BIOC_VERSION/bioc"
+
+# Control generation of the report:
+export BBS_REPORT_NODES="biocgpu"
+export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
+export BBS_REPORT_CSS="$BBS_HOME/$BBS_BIOC_VERSION/report.css"
+export BBS_REPORT_BGIMG="$BBS_HOME/images/DEVEL3b.png"
+export BBS_REPORT_JS="$BBS_HOME/$BBS_BIOC_VERSION/report.js"
+#export BBS_REPORT_MOTD="Happy new year to all Bioconductor developers!"
+
+# Control where to publish the report:
+export BBS_PUBLISHED_REPORT_RELATIVEURL="checkResults/$BBS_BIOC_VERSION/$BBS_BUILDTYPE-LATEST/"
+export BBS_PUBLISHED_REPORT_DEST_DIR="webadmin@master.bioconductor.org:/extra/www/bioc/$BBS_PUBLISHED_REPORT_RELATIVEURL"
+
+
+# -----------------------------------------------------------------------------
+# The variables below control stage7-notify.sh so only need to be defined on
+# the central node
+
+# TODO: when BBS_NOTIFY_NODES is not defined then take all the build nodes
+export BBS_NOTIFY_NODES="biocgpu"
+export BBS_PUBLISHED_REPORT_URL="https://bioconductor.org/$BBS_PUBLISHED_REPORT_RELATIVEURL"
+
