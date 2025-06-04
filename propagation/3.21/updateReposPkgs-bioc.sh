92af6a72909a83a93402e915d76c9516e001ead3
A Wokaty <awokaty@users.noreply.github.com>
2025-06-04 09:54:45 -0400

    Remove meta db creation for 3.21

--- propagation/3.21/updateReposPkgs-bioc.sh
+++ propagation/3.21/updateReposPkgs-bioc.sh
@@ -17,10 +17,6 @@ WIN_CONTRIB="$REPOS_ROOT/bin/windows/contrib/$R_VERSION"
 MAC_BIG_SUR_x86_64_CONTRIB="$REPOS_ROOT/bin/macosx/big-sur-x86_64/contrib/$R_VERSION"
 MAC_BIG_SUR_arm64_CONTRIB="$REPOS_ROOT/bin/macosx/big-sur-arm64/contrib/$R_VERSION"
 
-META_SRC="$SRC_CONTRIB/Meta"
-META_R_EXPR="source('/home/biocbuild/BBS/utils/makeMetaDbs.R')"
-MEAT_PATH="/home/biocbuild/bbs-$BIOC_VERSION-bioc/meat"
-
 if [ ! -f "$PROPAGATION_DB_FILE" ]; then
         echo "ERROR: $PROPAGATION_DB_FILE not found. Did postrun.sh run?"
         exit 1
@@ -76,13 +72,6 @@ for i in `ls $MANUALS_SRC`; do
 	mkdir -p $MANUALS_DEST/$pkg/man
 	cp --update --verbose $MANUALS_SRC/$i $MANUALS_DEST/$pkg/man
 done
-echo ""
-echo "========================================================================"
-/bin/date
-echo "------------------------------------------------------------------------"
-
-echo "Updating $META_SRC with aliases and cross refences dbs..."
-$Rscript -e "$META_R_EXPR; try(makeMetaDbs('$PROPAGATION_DB_FILE', '$MEAT_PATH', '$REPOS_ROOT', '$META_SRC'))"
 
 echo "DONE."
 exit 0
