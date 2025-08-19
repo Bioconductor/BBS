4373c6e9b232da3c3e242568b67b32b62697518e
Hervé Pagès <hpages.on.github@gmail.com>
2025-08-19 16:23:03 -0700

    update OS version for teran2

--- nodes/nodespecs.py
+++ nodes/nodespecs.py
@@ -34,7 +34,7 @@ allnodes = {
                     'Platform': "x86_64-linux-gnu",
                     'pkgType' : "source",
                     'encoding': "utf_8"},
-    'teran2':      {'OS'      : "Linux (Ubuntu 24.04.2 LTS)",
+    'teran2':      {'OS'      : "Linux (Ubuntu 24.04.3 LTS)",
                     'Arch'    : "x86_64",
                     'Platform': "x86_64-linux-gnu",
                     'pkgType' : "source",
