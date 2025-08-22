5ae9f130f6b65f0ee0053b55ef9042e0e00a8dd6
Hervé Pagès <hpages.on.github@gmail.com>
2025-08-21 22:46:41 -0700

    Reconfigure the 3.22 books builds
    - Use bbscentral2 as central builder;
    - Add kjohnson3 to these builds (in addition to nebbiolo2).

--- 3.22/bioc-longtests/nebbiolo2/prerun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. ./config.sh
-
-set -e # Exit immediately if a simple command exits with a non-zero status.
-
-$BBS_PYTHON_CMD $BBS_HOME/BBS-prerun.py make-central-rdir
-$BBS_PYTHON_CMD $BBS_HOME/BBS-prerun.py upload-meat-info
