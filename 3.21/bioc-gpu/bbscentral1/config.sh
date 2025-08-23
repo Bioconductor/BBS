017200604196cbec3a3062f82c8631b7ddd72270
A Wokaty <awokaty@users.noreply.github.com>
2025-08-22 21:59:55 -0400

    Fix BBS_R_HOME for bbscentrals* bioc-gpu build

--- 3.21/bioc-gpu/bbscentral1/config.sh
+++ 3.21/bioc-gpu/bbscentral1/config.sh
@@ -12,7 +12,7 @@ export BBS_DEBUG="0"
 export BBS_NODE_HOSTNAME="bbscentral1"
 export BBS_USER="biocbuild"
 export BBS_WORK_TOPDIR="/home/biocbuild/bbs-3.21-bioc-gpu"
-export BBS_R_HOME="$BBS_WORK_TOPDIR/R"
+export BBS_R_HOME="/home/biocbuild/R/R-4.5"
 export R_LIBS="$BBS_R_HOME/site-library"
 
 export BBS_CENTRAL_RHOST="localhost"
