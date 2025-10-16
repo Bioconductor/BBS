f05c5b3babe137d2bdc99f98982a18878858fc59
A Wokaty <awokaty@users.noreply.github.com>
2025-10-16 15:22:59 -0400

    Fix config

--- 3.23/bioc-gpu/config.sh
+++ 3.23/bioc-gpu/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ==========================================================================
-# Settings shared by all the Unix nodes involved in the 3.22-bioc-gpu builds
+# Settings shared by all the Unix nodes involved in the 3.23-bioc-gpu builds
 # ==========================================================================
 
 
@@ -30,7 +30,7 @@ export BBS_BIOC_MANIFEST_FILE="software.txt"
 # Where is the fresh meat to be stored by prerun (stage1)
 export BBS_MEAT0_RHOST="$BBS_CENTRAL_RHOST"
 export BBS_MEAT0_RUSER="$BBS_CENTRAL_RUSER"
-export BBS_MEAT0_RDIR="/home/biocbuild/bbs-3.22-bioc-gpu/MEAT0"
+export BBS_MEAT0_RDIR="/home/biocbuild/bbs-3.23-bioc-gpu/MEAT0"
 
 # Triggers a MEAT0 update at beginning of prerun (stage1)
 export BBS_UPDATE_MEAT0=1
