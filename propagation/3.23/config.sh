724e2c8cdcf306b46d4970f28d7af9906b111c32
A Wokaty <awokaty@users.noreply.github.com>
2025-10-20 10:48:47 -0400

    Add propagation/3.23

---
+++ propagation/3.23/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# =========================================================
+# Settings shared by all scripts in propagation/3.23 folder
+# =========================================================
+
+BIOC_VERSION="3.23"
+R_VERSION="4.6"
+Rscript="$HOME/bin/Rscript-$R_VERSION"
---
+++ propagation/3.23/prepareRepos-workflows.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd "$HOME/propagation/3.23"
+
+. ./config.sh
+. ./prepareRepos-workflows.config
+. ./prepareRepos.sh
+
