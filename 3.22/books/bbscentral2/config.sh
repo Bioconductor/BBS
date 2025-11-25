5a039518d8358387b3527bbf8ac58388cef5e37f
A Wokaty <awokaty@users.noreply.github.com>
2025-11-25 10:15:47 -0500

    Remove kjohnson1 from 3.22 book builds

--- 3.22/books/bbscentral2/config.sh
+++ 3.22/books/bbscentral2/config.sh
@@ -38,7 +38,7 @@ export BBS_OUTGOING_MAP="source:nebbiolo2/buildsrc"
 export BBS_FINAL_REPO="file://home/biocpush/PACKAGES/$BBS_BIOC_VERSION/books"
 
 # Control generation of the report:
-export BBS_REPORT_NODES="nebbiolo2 merida1 kjohnson1"
+export BBS_REPORT_NODES="nebbiolo2 merida1"
 export BBS_REPORT_PATH="$BBS_CENTRAL_RDIR/report"
 export BBS_REPORT_CSS="$BBS_HOME/$BBS_BIOC_VERSION/report.css"
 export BBS_REPORT_BGIMG="$BBS_HOME/images/DEVEL3b.png"
