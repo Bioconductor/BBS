9109438add50983fa7e9eb6dd7e79d29abbe9c31
A Wokaty <awokaty@users.noreply.github.com>
2026-01-29 21:30:36 -0500

    Add small formatting

--- BBS-report.py
+++ BBS-report.py
@@ -1184,7 +1184,7 @@ def write_gcard(out, pkg, pkg_pos, nb_pkgs, topdir, leafreport_ref,
         out.write('<TR>')
         out.write('<TD ROWSPAN="2" class="leftmost bottom_left_corner"></TD>')
         subdomain = "bioc" if BBSvars.git_branch == "devel" else "bioc-release"
-        out.write('<TD COLSPAN="7" class="special-content">See <A HREF="https://%s.r-universe.dev/%s">other builds for %s</A> in R Universe.</TD>'
+        out.write('<TD COLSPAN="7" class="special-content" valign="bottom">See <A HREF="https://%s.r-universe.dev/%s">other builds for %s</A> in R Universe.</TD>'
                   % (subdomain, pkg, pkg))
         out.write('<TD ROWSPAN="2" class="rightmost bottom_right_corner"></TD>')
         out.write('</TR>\n')
