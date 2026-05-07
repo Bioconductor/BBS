234ecac613a97514f1acfcc53da5208cec1ed1ca
Hervé Pagès <hpages.on.github@gmail.com>
2026-05-07 15:30:10 -0700

    increase BIOCCHECK timeout limit from 5 to 10 min

--- BBSvars.py
+++ BBSvars.py
@@ -75,7 +75,7 @@ if buildtype == 'books':
 else:
     default_CHECK_timeout = default_BUILD_timeout
 
-default_BIOCCHECK_timeout = '300.0'  # 5 min
+default_BIOCCHECK_timeout = '600.0'  # 10 min
 
 INSTALL_timeout   = float(BBSutils.getenv('BBS_INSTALL_TIMEOUT',  False,
                                           default_INSTALL_timeout))
